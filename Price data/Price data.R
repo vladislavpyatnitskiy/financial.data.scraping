d504c8c4f02b0155a9f197cc1aff4411b472a995
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-03 02:02:54 +0500

    Update Price data.R
    Added if else statements so now it is more convenient to set up start and end dates

--- Price data/Price data.R
+++ Price data/Price data.R
@@ -1,26 +1,44 @@
 # Access necessary libraries
-lapply(c("quantmod", "timeSeries"),
-       require, character.only = TRUE)
-
+lapply(c("quantmod", "timeSeries"), require, character.only = TRUE)
 # Type tickers
 tickers <- c("AMZN", "GOOGL")
 
 # Type start date
 start_date <- "2020-02-20"
 
+# Type end date
+end_date <- NULL
+
 # Set up function
-prices_from_yahoo <- function(y, z){
+prices_from_yahoo <- function(y, z = NULL, i = NULL){
   # Create an empty variable
   portfolioPrices <- NULL
-  
   # Loop for data extraction
   for (Ticker in y){
-    # fill the variable with dataset
-    portfolioPrices <- cbind(portfolioPrices,
-                             getSymbols(Ticker, from = z, src = "yahoo",
-                                        auto.assign=FALSE)[,4])
+    # When neither start date nor end date are defined
+    if (is.null(z) && is.null(i)) {
+      # fill the variable with dataset
+      portfolioPrices <- cbind(portfolioPrices,
+                                getSymbols(Ticker, src = "yahoo",
+                                          auto.assign=FALSE)[,4])
+      } else if (is.null(i)) {
+      # fill the variable with dataset
+      portfolioPrices <- cbind(portfolioPrices,
+                                getSymbols(Ticker, from = z, src = "yahoo",
+                                          auto.assign=FALSE)[,4])
+      } else if (is.null(z)) {
+      # fill the variable with dataset
+      portfolioPrices <- cbind(portfolioPrices,
+                                 getSymbols(Ticker, to = i, src = "yahoo",
+                                          auto.assign=FALSE)[,4])
+      } else { 
+      # fill the variable with dataset
+      portfolioPrices <- cbind(portfolioPrices,
+                                 getSymbols(Ticker, from = z, to = i,
+                                          src = "yahoo", 
+                                          auto.assign=FALSE)[,4])
+    }
   }
-  
   # Get rid of NAs
   portfolioPrices <- portfolioPrices[apply(portfolioPrices,1,
                                            function(x) all(!is.na(x))),]
@@ -37,5 +55,5 @@ prices_from_yahoo <- function(y, z){
   return(portfolioReturns)
 }
 
-# Type variables into function
-prices_from_yahoo(tickers, start_date)
+stock_data <- prices_from_yahoo(tickers, start_date)
+stock_data
