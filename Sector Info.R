4384f78afb50ffca40bcf0ef426a6e97d2b79cb4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-18 07:15:16 +0500

    Create Sector Info.R
    Added script to get info about stocks sector info

---
+++ Sector Info.R
@@ -0,0 +1,23 @@
+c.sector <- function(x){ # Function to get info about company type
+  
+  l <- NULL # Create list
+  
+  for (n in 1:length(x)){ s <- x[n] # For each security find sector
+  
+    p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", s, s)
+    
+    page.p <- read_html(p) # Read HTML & extract necessary info
+    
+    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab11
+    
+    yahoo.header1 <- tab11 %>% html_nodes('p') %>% html_nodes('span') %>%
+      html_text()
+    
+    l <- rbind(l, yahoo.header1[2])} # Add to list
+  
+  colnames(l) <- "Sector" # 
+  rownames(l) <- x #
+  
+  l # Display
+}
+c.sector(x = c("AAPL", "NRG", "PVH", "C")) # Test
