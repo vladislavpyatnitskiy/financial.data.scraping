1ed59952e14dd0850fc2b6944c63ae70e189b8ae
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-20 18:42:20 +0500

    Update Statistics From Yahoo.R
    Upgraded script:
    
    • Added option to scrape info of several stocks simultaneously 
    • Added option to transpose

--- Statistics From Yahoo.R
+++ Statistics From Yahoo.R
@@ -1,33 +1,40 @@
 # Function to Scrape Statistics for Security
-statistics.yahoo <- function(x){ # Security URL
+statistics.yahoo <- function(x, transpose = F){ # Security URL
   
-  s <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s", x, x)
+  df.s <- NULL # Set up list for infos
   
-  s.page <- read_html(s) # Read HTML of page
+  for (n in 1:length(x)){ j <- x[n] # For each security get info
   
-  s.yahoo <- s.page %>% html_nodes('table') %>% .[[1]] -> tab1 # Assign Table 
-  
-  s.header <- tab1 %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  df.f1 <- NULL # Create lists for ratio names and values
-  df.f2 <- NULL
-  
-  for (n in 0:(length(s.header) / 2)){ 
+    s <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",j,j)
     
-    df.f1 <- rbind(df.f1, s.header[(1 + n * 2)]) # Ratio names
+    s.page <- read_html(s) # Read HTML of page
     
-    df.f2 <- rbind(df.f2, s.header[(2 + n * 2)]) } # Ratio values
+    s.yahoo <- s.page %>% html_nodes('table') %>% .[[1]] -> tab1 # Assign Table 
+    
+    s.header <- tab1 %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    
+    df.f1 <- NULL # Create lists for ratio names and values
+    df.f2 <- NULL
+    
+    for (n in 0:(length(s.header) / 2)){ 
+      
+      df.f1 <- rbind(df.f1, s.header[(1 + n * 2)]) # Ratio names
+      
+      df.f2 <- rbind(df.f2, s.header[(2 + n * 2)]) } # Ratio values
+    
+    df.f3 <- data.frame(df.f1, df.f2) # Join 
+    
+    df.f3 <- df.f3[-nrow(df.f3),] # Display
+    
+    rownames(df.f3) <- df.f3[,1] # Assign row names
+    
+    df.f3 <- subset(df.f3, select = -c(1)) # Reduce excess column
+    
+    colnames(df.f3) <- j # Assign column name
+    
+    if (is.null(df.s)){ df.s <- df.f3 } else { df.s <- cbind(df.s, df.f3) } }
   
-  df.f3 <- data.frame(df.f1, df.f2) # Join 
-  
-  df.f3 <- df.f3[-nrow(df.f3),] # Display
-  
-  rownames(df.f3) <- df.f3[,1] # Assign row names
-  
-  df.f3 <- subset(df.f3, select = -c(1)) # Reduce excess column
-  
-  colnames(df.f3) <- x # Assign column name
-  
-  df.f3 # Display value
+  if (isTRUE(transpose)){ t(df.s) } else { df.s } # Display
 }
-statistics.yahoo("AAPL") # Test
+# Test
+statistics.yahoo(x = c("AAPL", "MSFT", "GOOGL", "AMZN", "META"), transpose = F)
