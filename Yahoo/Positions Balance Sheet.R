79af1e1eb86aec9c8288a707fef7ea120e0f6bdd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-16 23:55:36 +0500

    Create Positions Balance Sheet.R
    Added script to check position in balance sheet

---
+++ Yahoo/Positions Balance Sheet.R
@@ -0,0 +1,21 @@
+bs.positions.yahoo <- function(x, c){ # Positions from Balance Sheet
+  
+  p <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s", x, x)
+  
+  page.p <- read_html(p) # Read HTML & extract necessary info
+  
+  price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab11
+  
+  y <- tab11 %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+  
+  y <- y[grep(c, y) + 1] # Take next position 
+  
+  y <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", y)) # Reduce commas
+  
+  y <- as.numeric(y)[1] # Make it numeric
+  
+  names(y) <- c # Give names
+  
+  y # Display
+}
+bs.positions.yahoo("AAPL", "Invested Capital") # Test
