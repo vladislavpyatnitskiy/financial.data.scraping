41534fda35d6fc1e1f086bbb881fb7a707312821
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-17 00:07:53 +0500

    Create Positions From Cash Flow Statement.R
    Added script to find values from cash flow statement

---
+++ Yahoo/Financial Statements/Positions From Cash Flow Statement.R
@@ -0,0 +1,21 @@
+cf.positions.yahoo <- function(x, c){ # Positions from Cash Flow Statement
+  
+  p <- sprintf("https://finance.yahoo.com/quote/%s/cash-flow?p=%s",x,x)
+  
+  page.p <- read_html(p) # Read HTML & extract necessary info
+  
+  price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab11
+  
+  y <- tab11 %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+  
+  y <- y[grep(c, y) + 1] # Take next position 
+  
+  y <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", y)) # Reduce commas
+  
+  y <- as.numeric(y)[1] # Make it numeric
+  
+  names(y) <- c # Give names
+  
+  y # Display
+}
+cf.positions.yahoo("AAPL", "Free Cash Flow") # Test
