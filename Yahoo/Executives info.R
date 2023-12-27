17d76a6d057efce90844b0f4fc37acbb07dd06ab
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-27 14:40:01 +0500

    Update Executives info.R
    Upgraded script by adding one more for loop that reduced excessive lines

--- Yahoo/Executives info.R
+++ Yahoo/Executives info.R
@@ -1,38 +1,23 @@
+library("rvest")
+
 c.executives <- function(x){ # Get info about executives
   
-  p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", x, x)
-    
-  page.p <- read_html(p) # Read HTML & extract necessary info
-    
-  price.yahoo1 <- page.p %>% html_nodes('table') %>% .[[1]] -> tab11
-    
-  yahoo.header1 <- tab11 %>% html_nodes('tr') %>% html_nodes('td') %>%
-    html_text()
+  p<-read_html(sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s",x,x))
   
-  df.f1 <- NULL
-  df.f2 <- NULL
-  df.f3 <- NULL
-  df.f4 <- NULL
-  df.f5 <- NULL
+  p <- p %>% html_nodes('table') %>% .[[1]] -> tab
   
-  for (n in 0:(length(yahoo.header1) / 5)){ # Create table
-    
-    df.f1 <- rbind(df.f1, yahoo.header1[(1 + n * 5)]) # Name
-    
-    df.f2 <- rbind(df.f2, yahoo.header1[(2 + n * 5)]) # Title
-    
-    df.f3 <- rbind(df.f3, yahoo.header1[(3 + n * 5)]) # Pay
-    
-    df.f4 <- rbind(df.f4, yahoo.header1[(4 + n * 5)]) # Exercised 
-    
-    df.f5 <- rbind(df.f5, yahoo.header1[(5 + n * 5)]) } # Year Born
+  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
   
-  df.f6 <- data.frame(df.f1, df.f2, df.f3, df.f4, df.f5) # Join 
+  df.f2 <- NULL # Create space for data frame
   
-  df.f6 <- df.f6[-nrow(df.f6),] # Subset excess row
+  for (n in 1:(length(y) / 10)){ c.df <- NULL # Structure Data
+    
+    for (m in seq(0, 9)){ c.df <- rbind(c.df, y[(n + m * 5)]) }
+    
+    if (is.null(df.f2)){ df.f2 <- c.df } else { df.f2 <- cbind(df.f2, c.df) } } 
   
-  colnames(df.f6) <- c("Name", "Title", "Pay", "Exercised", "Year Born")
+  colnames(df.f2) <- c("Name", "Title", "Pay", "Exercised", "Year Born")
   
-  df.f6 # Output
+  df.f2 # Output
 }
 c.executives("AAPL") # Test
