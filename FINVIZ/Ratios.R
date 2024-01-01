bd00b4206c64531a206163508d9c09b8cdd77317
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-01 12:16:55 +0500

    Update Ratios.R
    Optimised script:
    
    • Reduced excessive lines
    • Made FOR loop one liner
    • Renamed variables
    • Eliminated excessive lists

--- FINVIZ/Ratios.R
+++ FINVIZ/Ratios.R
@@ -1,36 +1,31 @@
-finviz.ratios <- function(x){ df.f4 <- NULL
-  
+library("rvest") # Library
+
+finviz.ratios <- function(x){ v <- NULL # Where to store values
+
   for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
   
     s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j))
     
-    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab1 # Assign Table 
+    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab # Assign Table 
     
-    s.header <- tab1 %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
     
-    df.f1 <- NULL # Create lists to contain values
-    df.f2 <- NULL
+    d <- NULL # Data Frame with values
     
-    for (n in 0:(length(s.header) / 2)){ 
-      
-      df.f1 <- rbind(df.f1, s.header[(1 + n * 2)]) # Ratio names
-      
-      df.f2 <- rbind(df.f2, s.header[(2 + n * 2)]) } # Ratio values
+    for (n in 0:(length(y) / 2)){ d <- rbind(d,cbind(y[(1+n*2)],y[(2+n*2)])) }
     
-    df.f3 <- data.frame(df.f1, df.f2) # Join 
+    d <- d[-nrow(d),] # Reduce last column
     
-    df.f3 <- df.f3[-nrow(df.f3),] # Reduce last column
+    d[27,1] <- "EPS next Y (%)" # Change name
     
-    df.f3[27,1] <- "EPS next Y (%)" # Change name
+    rownames(d) <- d[,1] # Assign row names
     
-    rownames(df.f3) <- df.f3[,1] # Assign row names
+    d <- subset(d, select = -c(1)) # Reduce excessive column
     
-    df.f3 <- subset(df.f3, select = -c(1)) # Reduce excess column
+    colnames(d) <- j # Assign column name
     
-    colnames(df.f3) <- j # Assign column name
+    if (is.null(v)){ v <- d } else { v <- cbind(v, d) } } # Join
     
-    if (is.null(df.f4)){ df.f4<-df.f3 } else { df.f4 <- cbind(df.f4,df.f3) } }
-  
-  df.f4 # Display
+  v # Display
 }
 finviz.ratios(x = c("AAPL", "AMZN", "GOOGL", "META", "MSFT")) # Test
