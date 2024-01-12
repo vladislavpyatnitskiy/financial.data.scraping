fc86501992d325114924cf8c06a7636432ef7d10
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-12 20:34:21 +0500

    Update Positions Income Statement.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined functions
    • Added library

--- Yahoo/Financial Statements/Positions Income Statement.R
+++ Yahoo/Financial Statements/Positions Income Statement.R
@@ -1,18 +1,17 @@
+library("rvest") # Library
+
 is.positions.yahoo <- function(x, c){ # Positions from Income Statement
   
   p <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", x, x)
   
   page.p <- read_html(p) # Read HTML & extract necessary info
   
-  price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab11
+  price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab
   
-  y <- tab11 %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
+  y <- tab %>% html_nodes('div') %>% html_nodes('span') %>% html_text()
   
-  y <- y[grep(c, y) + 1] # Take next position 
-  
-  y <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", y)) # Reduce commas
-  
-  y <- as.numeric(y)[1] # Make it numeric
+  # Find value, clean and make it numeric 
+  y <- as.numeric(gsub(",","", gsub("([a-zA-Z]),","\\1 ", y[grep(c,y)+1])))[1] 
   
   names(y) <- c # Give names
   
