d46c275006be6058cb6fa5a8d23bf8c87aaee992
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-13 14:11:25 +0500

    Update Statistics From Yahoo.R
    Optimised script by reducing an extra FOR loop

--- Yahoo/Statistics From Yahoo.R
+++ Yahoo/Statistics From Yahoo.R
@@ -7,32 +7,30 @@ statistics.yahoo <- function(x, y = 1, transpose = F){ # Security URL
   
   for (n in 1:length(x)){ j <- x[n] # For each security get info
   
-    s <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",j,j)
+  s <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",j,j)
+  
+  s.page <- read_html(s) # Read HTML of page
+  
+  s.yahoo <- s.page %>% html_nodes('table') %>% .[[y]] -> tab # Assign Table 
+  
+  s <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+  
+  df <- NULL # Data Frame values
+  
+  for (n in 0:(length(s) / 2)){ # Row Values
     
-    s.page <- read_html(s) # Read HTML of page
+    df <- rbind(df, cbind(s[(1 + n * 2)], s[(2 + n * 2)])) } # Ratio values
     
-    s.yahoo <- s.page %>% html_nodes('table') %>% .[[y]] -> tab # Assign Table 
-    
-    s <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-    
-    df.f2 <- NULL # Data Frame values
-    
-    for (n in 0:(length(s) / 2)){ df.f1 <- NULL # Row Values
-      
-      for (m in seq(2)){ df.f1 <- cbind(df.f1, s[(m + n * 2)]) }
-      
-      df.f2 <- rbind(df.f2, df.f1) } # Ratio values
-    
-    df.f2 <- df.f2[-nrow(df.f2),] # Display
-    
-    rownames(df.f2) <- df.f2[,1] # Assign row names
-    
-    df.f2 <- subset(df.f2, select = -c(1)) # Reduce excess column
-    
-    colnames(df.f2) <- j # Assign column name
-    
-    if (is.null(df.s)){ df.s <- df.f2 } else { df.s <- cbind(df.s, df.f2) } }
-    
+  df <- df[-nrow(df),] # Reduce excessive row
+  
+  rownames(df) <- df[,1] # Assign row names
+  
+  df <- subset(df, select = -c(1)) # Reduce excess column
+  
+  colnames(df) <- j # Assign column name
+  
+  if (is.null(df.s)){ df.s <- df } else { df.s <- cbind(df.s, df) } }
+  
   if (isTRUE(transpose)){ t(df.s) } else { df.s } # Display
 }
 # Test
