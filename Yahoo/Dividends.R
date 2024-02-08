875afebcf299c97b5ec3a493e8e001f41b02176d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-08 15:52:14 +0500

    Update Dividends.R
    Upgraded script:
    
    • Stocks without dividends can be reflected into DataFrame with zeros

--- Yahoo/Dividends.R
+++ Yahoo/Dividends.R
@@ -2,62 +2,111 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libs
 
 s.dividends <- function(x, s = NULL, e = NULL){ # Dividends data
   
-  d <- NULL # Create an empty variable
+  d <- NULL # Create an empty variables
+  no.d <- NULL
   
   for (A in x){ if (is.null(s) && is.null(e)) { # When no start or end date
     
-    if (is.null(d)){ d <- getDividends(A)
+    if (is.null(d)){ d <- getDividends(A) # Get dividends or add to new list
         
-        r.names <- rownames(d)
+    if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
+    
+        r.names <- rownames(d) # Subtract dates
         
-        d <- cbind(r.names, d) } else { d2 <- getDividends(A)
+        d <- cbind(r.names, d) } } else { d2 <- getDividends(A)
         
-        r.names2 <- rownames(d2)
+        if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
         
-        d2 <- cbind(r.names2, d2)
+        r.names2 <- rownames(d2) # Subtract dates
         
-        d<-merge(d, d2, all=T) } # When there is start date:
+        d2 <- cbind(r.names2, d2) # Join
+        
+        d<-merge(d, d2, all=T) } } # When there is start date:
     
     } else if (is.null(e)) { if (is.null(d)){ d<-getDividends(A,from=s)
         
-    r.names <- rownames(d)
+    if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
     
-    d <- cbind(r.names, d) } else { d2 <- getDividends(A,from=s)
+    r.names <- rownames(d) # Subtract dates
+    
+    d <- cbind(r.names, d) } } else { d2 <- getDividends(A,from=s)
       
-      r.names2 <- rownames(d2)
+    if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
+    
+      r.names2 <- rownames(d2) # Subtract dates
       
-      d2 <- cbind(r.names2, d2)
+      d2 <- cbind(r.names2, d2) # Join
       
-      d<-merge(d, d2, all=T) } # When there is end date:
+      d<-merge(d, d2, all=T) } } # When there is end date:
       
     } else if (is.null(s)) { if (is.null(d)){ d<-getDividends(A,to=e)
-       
-    r.names <- rownames(d)
     
-    d <- cbind(r.names, d) } else { d2 <- getDividends(A,to=e)
+    if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
+    
+    r.names <- rownames(d) # Subtract dates
+    
+    d <- cbind(r.names, d) } } else { d2 <- getDividends(A,to=e)
           
-      r.names2 <- rownames(d2)
+    if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
+    
+      r.names2 <- rownames(d2) # Subtract dates
       
-      d2 <- cbind(r.names2, d2)
+      d2 <- cbind(r.names2, d2) # Join
       
-      d<-merge(d, d2, all=T) } # When there are both start and end dates:
+      d<-merge(d, d2, all=T) } } # When there are both start and end dates:
     
     } else { if (is.null(d)){ d<-getDividends(A,from=s,to=e)
-      
-    r.names <- rownames(d)
     
-    d <- cbind(r.names, d) } else { d2<-getDividends(A,from=s,to=e)
+    if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
+      
+    r.names <- rownames(d) # Subtract dates
+    
+    d <- cbind(r.names, d) } } else { d2<-getDividends(A,from=s,to=e)
         
-      r.names2 <- rownames(d2)
+      if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
+    
+      r.names2 <- rownames(d2) # Subtract dates
       
-      d2 <- cbind(r.names2, d2)
+      d2 <- cbind(r.names2, d2) # Join
       
-      d<-merge(d, d2, all=T) } } } # End of data collection
+      d<-merge(d, d2, all=T) } } } } # End of data collection
   
   d[is.na(d)] <- 0 # Substitute NA with 0
   
-  colnames(d) <- x # Assign column names
+  d <- as.timeSeries(d) # Make them Time Series
   
-  as.timeSeries(d) # Make them Time Series
+  r.names3 <- as.data.frame(rownames(d)) # Transfrom data into Data Frame
+  
+  r.names5 <- NULL # Create Data Frame for Stocks without dividends
+  
+  for (n in 1:length(no.d)){ r.names4 <- data.frame(r.names3, 0)
+    
+    colnames(r.names4) <- c("Date", no.d[n])
+    
+    if (is.null(r.names5)){ r.names5 <- r.names4 } else {
+      
+      r.names5 <- merge(r.names5, r.names4, by = "Date") } }
+  
+  rownames(r.names5) <- r.names5[,1] # Put dates into row names
+  
+  r.names5 <- r.names5[,-1] # Reduce dates column
+  
+  d <- cbind(d, as.timeSeries(r.names5)) # Join
+  
+  l <- NULL # Create empty list to clean column names
+  
+  for (n in 1:length(colnames(d))){
+    
+    if (isTRUE(grepl("div", colnames(d)[n], fixed = T))){ 
+      
+      l <- c(l, gsub('[".div"]','', colnames(d)[n])) #
+      
+      } else { l <- c(l, colnames(d)[n]) } } #
+  
+  colnames(d) <- l # Put clean names to Data Frame
+  
+  d <- d[,x] # Change order of columns
+
+  d # Display
 }
-s.dividends(c("AAPL", "MSFT"), s = "2023-01-01") # Test
+s.dividends(c("AAPL","MSFT","AMZN","C","GOOGL","VSTO"),s="2022-07-19") # Test
