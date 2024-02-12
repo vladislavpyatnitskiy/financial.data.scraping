2b7aec2418f89ab28c69e057fc7e47162d90e2e3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-12 14:22:32 +0500

    Create Company Country Info.R
    Added script to get info about company's country

---
+++ Yahoo/Company Country Info.R
@@ -0,0 +1,23 @@
+library("rvest") # Library
+
+c.country <- function(x){ l <- NULL # Create list
+
+  for (n in 1:length(x)){ s <- x[n] # For each security find sector
+  
+    p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", s, s)
+    
+    page.p <- read_html(p) # Read HTML & extract necessary info
+    
+    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab
+    
+    y <- tab %>% html_nodes('p') # Find country character in elements
+    
+    l <- rbind(l, strsplit(toString(y),
+                           "<br>")[[1]][length(strsplit(toString(y),
+                                                        "<br>")[[1]])-4]) }
+  colnames(l) <- "Country"
+  rownames(l) <- x
+  
+  l # Display
+}
+c.country(c("STLA", "ZIM", "AMZN", "VALE")) # Test
