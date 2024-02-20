dff7963098da35c228288ddde6357ca813b53e3f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-20 15:36:37 +0500

    Update Smartlab Stock Screener.R
    Fixed number in loop that made column numeric from 2nd column instead of making numeric from first column

--- Smart-Lab/Smartlab Stock Screener.R
+++ Smart-Lab/Smartlab Stock Screener.R
@@ -1,12 +1,12 @@
 # Use Data Frame from function enabling to get data from Smartlab website
 smartlab.screener <- function(x){ # Screener for stocks
   
-  for (n in 2:ncol(x)){ x[,n] <- as.numeric(x[,n]) } # Make data numeric
+  for (n in 1:ncol(x)){ x[,n] <- as.numeric(x[,n]) } # Make data numeric
   
-  # Screener settigns
-  sm.model1 <- x[x$`EV/EBITDA` < 10 & x$`Debt/EBITDA` < 2 & x$`P/E` < 5 &
-                   x$`P/BV` < 1 & x$`EV/EBITDA` > 0 & x$`Debt/EBITDA` > 0 &
-                   x$`P/BV` > 0 & x$`P/E` > 0 & x$`P/S` < 1 & x$`P/S` > 0,]
+  # Screener settings
+  sm.model1 <- x[x$`EV/EBITDA` < 10 & x$`DEBT/EBITDA` < 4 & x$`P/E` < 20 &
+                   x$`P/BV` < 3 & x$`P/BV` > 0 & x$`P/E` > 0 & x$`P/S` < 2 &
+                   x$`P/S` > 0,]
   
   na.omit(sm.model1) # Display clean data
 }
