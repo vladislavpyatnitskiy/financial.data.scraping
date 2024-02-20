ac66e82286ca1f981bb72b36bbcafb2c9ffab1c6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-20 14:33:32 +0500

    Update Ratios data.R
    Optimised script:
    
    • Set tickers as row names
    • Column names are set up from html names of ratios

--- Smart-Lab/Ratios data.R
+++ Smart-Lab/Ratios data.R
@@ -1,31 +1,35 @@
 library("rvest") # Library
 
-smartlab.ratios <- function(x, c){ # Function to get info from Smartlab
+smartlab.ratios <- function(x){ # Function to get info from Smartlab
   
-  l <- NULL #
+  l <- NULL # Store data here
   
-  for (m in 1:length(x)){ v <- x[m] #
+  for (m in 1:length(x)){ v <- x[m] # For each ratio get Smartlab HTML
   
     s<-read_html(sprintf("https://smart-lab.ru/q/shares_fundamental/?field=%s",
-                         v)) # Smartlab HTML
+                         v))
     
     s.yahoo <- s %>% html_nodes('table') %>% .[[1]] -> tab
     
     y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
     
-    df <- NULL # Variable name for values
+    df <- NULL # Variable for Table with Name, Ticker and values
     
-    # Table with Name, Ticker and values
-    for (n in 0:(length(y)/6)){ df <- rbind(df,cbind(y[(3+n*6)],y[(6+n*6)])) }
+    for (n in 0:(length(y)/6)){ df <- rbind(df, cbind(y[(3+n*6)],y[(6+n*6)])) }
     
     df <- df[-nrow(df),] # Reduce last row
-    df[,2] <- as.numeric(gsub('["\t\n"]','',df[,2])) # Reduce characters
-    colnames(df) <- c("Ticker", c[m]) # Column names
+    df[,2] <- as.numeric(gsub('["\t\n"]', '', df[,2])) # Reduce characters
+    colnames(df) <- c("Ticker", gsub("_", "/", toupper(x[m]))) # Column names
     
     # Join
     if (is.null(l)){ l<-df } else { l<-merge(x=l,y=df,by="Ticker",all=T) } } 
     
-  l # Display
+  if (isTRUE(l[1,1] == "")){ l <- l[-1,] } # Reduce empty row
+  
+  rownames(l) <- l[,1] # Move tickers to row names
+  
+  l <- l[,-1] # Reduce excessive column with tickers which are in row names
+  
+  return(l) # Display
 }
-View(smartlab.ratios(x = c("ev_ebitda", "p_bv", "debt_ebitda", "p_e"),
-                     c = c("EV/EBITDA", "P/BV", "Debt/EBITDA", "P/E")))
+smartlab.ratios(x=c("ev_ebitda","p_bv","debt_ebitda","p_e","market_cap","p_s"))
