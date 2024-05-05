ddc890f134f3f8f1a5edeea8272cac57269352ca
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-05 21:26:47 +0500

    Update SmartLab Fundamentals.R
    Fixed bug leading to an error when getting data of tickers like "OZON" where firm name is similar to ticker name

--- Smart-Lab/SmartLab Fundamentals.R
+++ Smart-Lab/SmartLab Fundamentals.R
@@ -1,35 +1,41 @@
 library("rvest") # Library
 
-smartlab.ticker <- function(x, y){ # Fundamentals 
+smartlab.ticker <- function(x, y){ # Fundamental 
   
   l <- NULL # Store data here
   
   for (j in 1:length(x)){ k <- NULL
-    
-    for (m in 1:length(y)){ v <- y[m] # Fundamental
   
-      s <- read_html(sprintf("https://smart-lab.ru/q/%s/?field=%s",
-                             "shares_fundamental", v))
-      
-      s.yahoo <- s %>% html_nodes('table') %>% .[[1]] -> tab
-      
-      i <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    for (m in 1:length(y)){ v <- y[m] # Fundamental
     
-      df <- cbind(x[j], i[grep(x[j], i) + 3]) # Assign value to ticker
-      
-      df[,2] <- gsub('["\n"]', '', gsub('["\t"]', '', df[,2])) # Clean
-      
-      if (isTRUE(grepl(" ", df[,2]))){ df[,2] <- gsub(" ", "", df[,2]) }  
-      
-      colnames(df) <- c("Ticker", gsub("_", "/", toupper(y[m]))) # Column names
-      
-      if (is.null(k)){ k<-df } else { k<-merge(x=k,y=df,by="Ticker",all=T) } }
-      
+    s <- read_html(sprintf("https://smart-lab.ru/q/%s/?field=%s",
+                           "shares_fundamental", v))
+    
+    s.yahoo <- s %>% html_nodes('table') %>% .[[1]] -> tab
+    
+    i <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    
+    D <- NULL
+    
+    for (n in 0:(length(i)/6)){ D <- rbind(D, cbind(i[(3+n*6)], i[(6+n*6)])) }
+    
+    D <- cbind(x[j], D[D[,1] == x[j], 2]) 
+    
+    D[,2] <- gsub('["\n"]', '', gsub('["\t"]', '', D[,2])) # Clean
+    
+    if (isTRUE(grepl(" ", D[,2]))){ D[,2] <- gsub(" ", "", D[,2]) }  
+    
+    colnames(D) <- c("Ticker", gsub("_", "/", toupper(y[m]))) # Column names
+    
+    if (is.null(k)){ k<-D } else { k<-merge(x=k,y=D,by="Ticker",all=T) } }
+    
     if (is.null(l)){ l <- k } else { l <- rbind(l, k) } }   # Join
-      
+  
   if (isTRUE(l[1,1] == "")){ l <- l[-1,] } # Reduce empty row
   
   l <- as.data.frame(l[,-1]) # Reduce excessive column with tickers
+  
+  if (ncol(l) == 1){ colnames(l) <- gsub("_", "/", toupper(y)) } 
   
   rownames(l) <- x # Assign tickers
   
