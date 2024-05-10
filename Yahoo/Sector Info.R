ce42a00e9aa1661fde640b5afdd66d29b632d2e8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-10 12:28:52 +0500

    Update Sector Info.R
    Optimised script:
    
    • Substituted US version of Yahoo! Finance to UK one which allows parsing
    • Reduced excessive lines

--- Yahoo/Sector Info.R
+++ Yahoo/Sector Info.R
@@ -1,19 +1,17 @@
 library("rvest") # Library
 
 c.sector <- function(x){ l <- NULL # Create list
-  
+
   for (n in 1:length(x)){ s <- x[n] # For each security find sector
   
-    p <- sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s", s, s)
+    p <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/profile", s))
     
-    page.p <- read_html(p) # Read HTML & extract necessary info
-    
-    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab
+    Y <- p %>% html_nodes('div') %>% .[[1]] -> tab
     
     y <- tab %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
     
     l <- rbind(l, y[grep("Sector", y) + 1]) } # Add to list
-
+    
   colnames(l) <- "Sector" # 
   rownames(l) <- x #
   
