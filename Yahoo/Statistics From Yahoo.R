ef90b62da9ec0e19ef98c2262a11feeb306c96d1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-17 10:55:57 +0500

    Update Statistics From Yahoo.R
    Optimised script:
    
    • Renamed variables
    • Reduced excessive lines

--- Yahoo/Statistics From Yahoo.R
+++ Yahoo/Statistics From Yahoo.R
@@ -1,37 +1,34 @@
 library("rvest") # Library
 
-# Function to Scrape Statistics for Security
-statistics.yahoo <- function(x, y = 1, transpose = F){ # Security URL
+statistics.yahoo <- function(x, y = 1, transpose = F){ # Statistics for Stocks
   
-  df.s <- NULL # Set up list for infos
+  DF <- NULL # Set up list for infos
   
   for (n in 1:length(x)){ j <- x[n] # For each security get info
   
-  s <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",j,j)
-  
-  s.page <- read_html(s) # Read HTML of page
-  
-  s.yahoo <- s.page %>% html_nodes('table') %>% .[[y]] -> tab # Assign Table 
-  
-  s <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  df <- NULL # Data Frame values
-  
-  for (n in 0:(length(s) / 2)){ # Row Values
+    s <- sprintf("https://uk.finance.yahoo.com/quote/%s/key-statistics?p=%s",
+                 j, j)
     
-    df <- rbind(df, cbind(s[(1 + n * 2)], s[(2 + n * 2)])) } # Ratio values
+    s.page <- read_html(s) # Read HTML of page
     
-  df <- df[-nrow(df),] # Reduce excessive row
-  
-  rownames(df) <- df[,1] # Assign row names
-  
-  df <- subset(df, select = -c(1)) # Reduce excess column
-  
-  colnames(df) <- j # Assign column name
-  
-  if (is.null(df.s)){ df.s <- df } else { df.s <- cbind(df.s, df) } }
-  
-  if (isTRUE(transpose)){ t(df.s) } else { df.s } # Display
+    s.yahoo <- s.page %>% html_nodes('table') %>% .[[y]] -> tab # Assign Table 
+    
+    s <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    
+    D <- NULL # Data Frame values, like names for ratios and numbers
+    
+    for (n in 0:(length(s)/2)){ D <- rbind(D, cbind(s[(1+n*2)], s[(2+n*2)])) } 
+    
+    D <- D[-nrow(D),] # Reduce excessive row
+    
+    rownames(D) <- D[,1] # Assign row names
+    
+    D <- subset(D, select = -c(1)) # Reduce excess column
+    
+    colnames(D) <- j # Assign column name
+    
+    if (is.null(DF)){ DF <- D } else { DF <- cbind(DF, D) } }
+    
+  if (isTRUE(transpose)){ t(DF) } else { DF } # Display
 }
-# Test
-statistics.yahoo(x = c("AAPL", "MSFT", "GOOGL", "AMZN", "META"),1, transpose=F)
+statistics.yahoo(x = c("AAPL", "MSFT"), 1, transpose = F) # Test
