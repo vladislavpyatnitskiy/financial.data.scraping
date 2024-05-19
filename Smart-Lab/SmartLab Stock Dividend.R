0a96963d812b2453e4ef82a07fea92d1d1fa3c13
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-19 12:46:19 +0500

    Update SmartLab Stock Dividend.R
    Upgraded script:
    
    • script showing only approved dividends
    • Added option to select start and end points of dividend payment period
    • Added option to get dividend flow for several stocks in one data frame

--- Smart-Lab/SmartLab Stock Dividend.R
+++ Smart-Lab/SmartLab Stock Dividend.R
@@ -1,54 +1,102 @@
 library("rvest")
 
-rus.div <- function(x, y){ # Dividends of Russian Stocks
+rus.dividends <- function(x, s = NULL, e = NULL){ # Dividends of Russian Stocks
   
-  L <- NULL
+  P <- NULL #
   
-  for (m in 1:length(y)){ a <- y[m] # Get data for each year
+  y <- seq("2023", from = "2014", by = 1) # Dates
   
-    p <- read_html(sprintf("https://smart-lab.ru/dividends/index?year=%s", a))
+  for (j in 1:length(x)){ L <- NULL # Data for each security
+  
+    v <- x[j] # One by one
     
-    div <- p %>% html_nodes('table') %>% .[[1]] -> tab
-    
-    f <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-    
-    df <- NULL # Variable for Table with Name, Ticker and values
-    
-    if (isTRUE(any(f == x))){ # Format info to Data Frame
+    for (m in 1:length(y)){ a <- y[m] # Get data for each year
       
-      for (n in 0:(length(f) / 11)){ if (isTRUE(f[(2 + n * 11)] == x)){
+      p <- read_html(sprintf("https://smart-lab.ru/dividends/index?year=%s",a))
+      
+      div <- p %>% html_nodes('table') %>% .[[1]] -> tab # Table
+      
+      f <- tab %>% html_nodes('tr') # Show rows of dividends
+      
+      l <- NULL # Show only approved dividends 
+      
+      for (n in 1:length(f)){ if (isTRUE(f[n] %>% html_attr('class') ==
+                                         "dividend_approved")){
         
-        df <- rbind.data.frame(df,
-                               cbind(f[7 + n * 11],
-                                     as.numeric(gsub(",", ".",
-                                                     f[4 + n * 11])))) } } }
-    else { next } # Next Year
-    
+          q <- f[n] %>% html_nodes('td') %>% html_text()
+          
+          l <- c(l, q) } } # Join rows of approved dividends
+      
+      df <- NULL # Variable for Table with Name, Ticker and values
+      
+      if (isTRUE(any(l == v))){ # Format info to Data Frame
+        
+        for (n in 0:(length(l) / 11)){ if (isTRUE(l[(2 + n * 11)] == v)){
+          
+            df <- rbind.data.frame(df,
+                                   cbind(l[7 + n * 11],
+                                         as.numeric(gsub(",", ".",
+                                                         l[4+n*11])))) } } }
+      else { next } # Next Year
+      
     ts <- df[,1] # Dates
     
     for (n in 1:length(ts)){ # Reform data format
       
-      Y <- as.character(read.fwf(textConnection(ts[n]),
-                                 widths=c(nchar(ts[n]) - 4, nchar(ts[n]) - 0),
-                                 colClasses = "character")[2]) # Year
+      Y<-as.character(read.fwf(textConnection(ts[n]),widths=c(nchar(ts[n])-4,
+                                                              nchar(ts[n])-0),
+                               colClasses = "character")[2]) # Year
       
-      M <- as.character(read.fwf(textConnection(ts[n]),
-                                 widths=c(nchar(ts[n]) - 7, nchar(ts[n]) - 8),
-                                 colClasses = "character")[2]) # Month
+      M<-as.character(read.fwf(textConnection(ts[n]),widths=c(nchar(ts[n])-7,
+                                                              nchar(ts[n])-8),
+                               colClasses = "character")[2]) # Month
       
-      d <- as.character(read.fwf(textConnection(ts[n]),
-                                 widths=c(nchar(ts[n]) - 8, nchar(ts[n]) - 10),
+      d<-as.character(read.fwf(textConnection(ts[n]),widths=c(nchar(ts[n])-8,
+                                                              nchar(ts[n])-10),
                                  colClasses = "character")[1]) # Day
       
       ts[n] <- paste(Y, M, d, sep = "-") } # Concatenate dates
     
     df <- as.data.frame(df[,-1]) # Reduce excessive column
     
-    rownames(df) <- ts # Dates
-    colnames(df) <- x 
+    df <- data.frame(ts, df) # Join time series and dividends
     
-    L <- rbind(L, df) } # Tickers
+    colnames(df) <- c("Date", v) # Column names
+    
+    L <- rbind(L, df) } # Join rows of data frames
+    
+    for (n in 1:nrow(L)){ while (isFALSE(L[n,1] == L[nrow(L),1])){
+      
+        if (isTRUE(L[n + 1,1] == L[n,1]) & isTRUE(L[n + 1,2] == L[n,2])){ 
+          
+          L <- L[-(n + 1),] } # Delete Rows with duplicates
+      
+        break } } # End loop when it is over
   
-  L # Display
+    if (is.null(P)){ P = L } else { P <- merge(x=P, y=L, by="Date", all=T) } }
+  
+  dates <- P[,1] # Put dates into separate column
+  
+  P <- as.data.frame(P[,-1]) # Reduce excessive column
+  
+  for (n in 1:length(dates)){ while (dates[n] != dates[length(dates)]){
+    
+      if (isTRUE(dates[n + 1] == dates[n])){ # Add 1 when Double Dividends
+      
+        dates[n + 1] <- paste(dates[n + 1], "1", sep = "") } #
+    
+      break } } # End when the loop is over
+  
+  rownames(P) <- dates # Dates
+  
+  P[is.na(P)] <- 0 # Substitute NA with Zero values
+  
+  if (is.null(s) && is.null(e)){ P } # When dates are not submitted 
+  
+  else if (is.null(s)){ P[rownames(P) < e,] } # When only End Date submitted
+  
+  else if (is.null(e)){ P[rownames(P) > s,] } # When only Start Date submitted
+  
+  else { P[rownames(P) < e & rownames(P) > s,] } # When both dates submitted
 }
-rus.div("PHOR", seq("2023", from = "2014", by = 1)) # Test
+rus.dividends(c("LKOH", "MAGN", "PHOR", "MGNT"), s = "2022-01-01") # Test
