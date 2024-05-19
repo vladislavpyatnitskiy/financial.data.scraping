277ade0b99b3d928cdbbd4adc06ee406b9f6671b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-19 13:13:52 +0500

    Update and rename Price data/Price Data For Certain Year.R to Yahoo/Price data/Price Data For Certain Year.R
    Moved script to new folder

--- Yahoo/Price data/Price Data For Certain Year.R
+++ Yahoo/Price data/Price Data For Certain Year.R
@@ -2,13 +2,12 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # libraries
 
 prices.yahoo.year <- function(x, y){ # Get stock price data for certain year
   
-  p <- NULL # Create an empty variable
+  p <- NULL # Data from first of January to 31 of December
   
-  # Loop for data extraction 
   for (A in x){ p <- cbind(p, getSymbols(A, from = sprintf("%s-01-01", y),
                                          to = sprintf("%s-12-31", y),
                                          src = "yahoo", auto.assign = F)[,4]) }
-
+  
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
   colnames(p) <- x # Put the tickers in data set
