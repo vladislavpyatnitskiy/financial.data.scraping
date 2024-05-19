b2bf7a32131aea5a1733f9d4edb2f74fb35c4765
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-19 12:40:33 +0500

    Update SmartLab Dividend Info.R
    Upgraded script:
    
    • Selected only approved dividends 
    • Swapped Ticker and Company name columns
    • Changed type of data frame format enabling numerical operations with numerical values (dividends, dividend yield, stock price)

--- Smart-Lab/SmartLab Dividend Info.R
+++ Smart-Lab/SmartLab Dividend Info.R
@@ -1,27 +1,44 @@
 library("rvest") # Library
 
-smartlab.dividend <- function(x){ # Get dividend info for certain year
+smartlab.dividend <- function(x){ # Dividends Data Frame for certain year
   
   p <- read_html(sprintf("https://smart-lab.ru/dividends/index?year=%s", x))
   
-  div <- p %>% html_nodes('table') %>% .[[1]] -> tab
+  div <- p %>% html_nodes('table') %>% .[[1]] -> tab # Table
   
-  f <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+  f <- tab %>% html_nodes('tr') # Dividends
+  
+  L <- NULL # Show only Dividend Payments that have been approved 
+  
+  for (n in 1:length(f)){ if (isTRUE(f[n] %>% html_attr('class') ==
+                                     "dividend_approved")){
+      
+      q <- f[n] %>% html_nodes('td') %>% html_text()
+      
+      L <- c(L, q) } } # Join rows of approved dividends
   
   df <- NULL # Variable for Table with Name, Ticker and values
   
-  for (n in 0:(length(f)/11)){ # Format info to Data Frame
+  for (n in 0:(length(L) / 11)){ # Data Frame with Dividend Info
     
-    df <- rbind(df, cbind(f[(1 + n * 11)], f[(2 + n * 11)], f[(4 + n * 11)],
-                          f[(5 + n * 11)], f[(7 + n * 11)], f[(8 + n * 11)],
-                          f[(9 + n * 11)], f[(10 + n * 11)]))}
+    P <- as.numeric(gsub(",",  ".", read.fwf(textConnection(L[(5+n*11)]),
+                                             widths=c(nchar(L[(5+n*11)])-1,1),
+                                             colClasses = "character")[,1]))
+    df <- rbind.data.frame(df,
+                           cbind(L[(2 + n * 11)], # Ticker
+                                 L[(1 + n * 11)], # Name of Company
+                                 as.numeric(gsub(",", ".",
+                                                 L[(4 + n * 11)])), # Dividend
+                                 P, # Dividend Yield
+                                 L[(7 + n * 11)], # Buy Before
+                                 L[(8 + n * 11)], # Closing Day
+                                 L[(9 + n * 11)], # Payment Date
+                                 as.numeric(gsub(",", ".",
+                                                 L[(10 + n * 11)])))) } # Price
   
-  rownames(df) <- df[,2] # Move ticker column to row names
-  df <- df[,-2] # Reduce excess ticker column
-  
-  colnames(df) <- c("Название","Стоимость дивидендов", "%", "Купить до",
-                    "День Закрытия Реестра", "Выплата До", "Цена")
-  
+  colnames(df) <- c("Тикер", "Название", "Стоимость дивидендов",
+                    "Доходность (%)", "Купить до", "День Закрытия Реестра",
+                    "Выплата До", "Цена")
   df # Display
 }
 smartlab.dividend("2022") # Test
