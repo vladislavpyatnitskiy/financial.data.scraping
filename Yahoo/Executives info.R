b14648ce5137c6a2d7b9f09f16fefd84b13c310d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-21 11:22:19 +0500

    Update Executives info.R
    Optimised script:
    
    • Updated link
    • Renamed variables

--- Yahoo/Executives info.R
+++ Yahoo/Executives info.R
@@ -2,22 +2,22 @@ library("rvest")
 
 c.executives <- function(x){ # Get info about executives
   
-  p<-read_html(sprintf("https://finance.yahoo.com/quote/%s/profile?p=%s",x,x))
+  p <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/profile", x))
   
-  p <- p %>% html_nodes('table') %>% .[[1]] -> tab
+  tab <- p %>% html_nodes('table') %>% .[[1]]
   
   y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
   
-  df.f2 <- NULL # Create space for data frame
+  D <- NULL # Create space for data frame
   
-  for (n in 1:(length(y) / 10)){ c.df <- NULL # Structure Data
+  for (n in 1:(length(y) / 10)){ d <- NULL # Structure Data
+  
+    for (m in seq(0, 9)){ d <- rbind(d, y[(n + m * 5)]) }
     
-    for (m in seq(0, 9)){ c.df <- rbind(c.df, y[(n + m * 5)]) }
-    
-    if (is.null(df.f2)){ df.f2 <- c.df } else { df.f2 <- cbind(df.f2, c.df) } } 
+    if (is.null(D)){ D <- d } else { D <- cbind(D, d) } } 
   
-  colnames(df.f2) <- c("Name", "Title", "Pay", "Exercised", "Year Born")
+  colnames(D) <- c("Name", "Title", "Pay", "Exercised", "Year Born")
   
-  df.f2 # Output
+  D # Output
 }
 c.executives("AAPL") # Test
