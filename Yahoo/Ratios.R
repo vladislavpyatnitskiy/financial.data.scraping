f8de88fc560609f47362e3d3c075ff3cc021383c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-05-22 11:17:54 +0500

    Update Ratios.R
    Updated script:
    
    • Substituted link by changing US version to UK one

--- Yahoo/Ratios.R
+++ Yahoo/Ratios.R
@@ -3,12 +3,11 @@ library("rvest") # Library
 ratios.yahoo <- function(x, y){ m <- NULL # List for values
 
   for (n in 1:length(x)){ s <- x[n] # For each stock find data
-  
-    p <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",s,s)
     
-    page.p <- read_html(p) # Read HTML & extract necessary info
+    p <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/%s", s,
+                           "key-statistics"))
     
-    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab
+    Y <- p %>% html_nodes('div') %>% .[[1]] -> tab
     
     i <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
     
@@ -16,11 +15,11 @@ ratios.yahoo <- function(x, y){ m <- NULL # List for values
     
     for (l in 1:length(y)){ k <- cbind(k, i[grep(y[l], i) + 1]) }
     
-    m <- rbind(m, k) } # Join data for all stocks
-  
+      m <- rbind(m, k) } # Join data for all stocks
+    
   rownames(m) <- x # Tickers
   colnames(m) <- y # Ratios
   
   m # Display values
 }
-ratios.yahoo(c("AAPL", "AMZN", "GOOGL"), c("Market Cap", "Price/Book"))
+ratios.yahoo(c("AAPL", "AMZN", "GOOGL"), c("Market cap", "Price/book"))
