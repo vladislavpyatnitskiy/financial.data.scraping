81b71b3ce9d19bc33f85bb25f942e14c7f6b18fe
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-06-30 11:49:34 +0500

    Create Country.R
    Added script to get country values for each stock via stock analysis website

---
+++ stockanalysis website/Country.R
@@ -0,0 +1,22 @@
+library("rvest") # Library
+
+stock.analysis.country <- function(x){ # Countries for stock
+  
+  l <- NULL # Store countries for each security
+  
+  for (n in 1:length(x)){ i <- x[n] # Get Country for each security
+  
+    p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/company/", i))
+    
+    tab <- p %>% html_nodes('table') %>% .[[1]] # Select appropriate table
+    
+    C <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text() 
+    
+    l <- rbind.data.frame(l, C[grep("Country", C) + 1]) } # Data Frame 
+    
+  rownames(l) <- x # Tickers
+  colnames(l) <- "Country" # Country column name
+  
+  l # Display
+}
+stock.analysis.country(c("AAPL", "ZIM", "STLA", "VALE")) # Test
