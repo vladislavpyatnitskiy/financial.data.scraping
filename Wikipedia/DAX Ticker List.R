16e1fe2dd08648c3044889b69e46babaf97b32b2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-01 20:08:54 +0500

    Update DAX Ticker List.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Wikipedia/DAX Ticker List.R
+++ Wikipedia/DAX Ticker List.R
@@ -2,18 +2,10 @@ library("rvest")
 
 dax.list.wiki <- function(x){ # List of tickers from German Index DAX
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # HTML to get data
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[5]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # Store tickers and clean data
-  
-  for (n in 0:(length(y) / 7)){ v <- rbind.data.frame(v, y[4 + n * 7]) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  v # Display
+  s[seq(from = 4, to = length(s), by = 7)] # Display tickers
 }
 dax.list.wiki("DAX") # Test
