67604da2de6637f76b78e46e4d907e9c2e05c74f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-01 20:14:09 +0500

    Update NASDAQ 100 List.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Wikipedia/NASDAQ 100 List.R
+++ Wikipedia/NASDAQ 100 List.R
@@ -2,20 +2,10 @@ library("rvest") # Library
 
 nasdaq.list.wiki <- function(x){ # Get data with tickers
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # HTML to get data
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[5]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL
-  
-  for (n in 0:(length(y) / 4)){ v <- rbind.data.frame(v, y[2 + n * 4]) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  v <- as.vector(v) # Change format to vector
-  
-  v # Display
+  s[seq(from = 2, to = length(s), by = 4)] # Display
 }
-nasdaq.list.wiki("Nasdaq-100")
+nasdaq.list.wiki("Nasdaq-100") # Test
