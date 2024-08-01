6c821cea92c2a4e04cc572e016ce3bd119db0659
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-01 20:23:24 +0500

    Update FTSE 100 Ticker List.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Wikipedia/FTSE 100 Ticker List.R
+++ Wikipedia/FTSE 100 Ticker List.R
@@ -2,20 +2,11 @@ library("rvest") # Library
 
 ftse100.list.wiki <- function(x, yahoo = T){ # Tickers from FTSE 100
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # HTML to get data
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[5]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # Store tickers
-  
-  for (n in 0:(length(y) / 3)){ v <- rbind.data.frame(v, y[2 + n * 3]) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  if (isTRUE(yahoo)){ v <- paste(v, ".L", sep = "") } # To get data via Yahoo
-  
-  v # Display
+  if (isTRUE(yahoo)){ paste(s[seq(from=2, to=length(s), by=3)], ".L", sep="")
+    } else { s[seq(from = 2, to = length(s), by = 3)] } # Display
 }
 ftse100.list.wiki("FTSE_100_Index", yahoo = T) # Test
