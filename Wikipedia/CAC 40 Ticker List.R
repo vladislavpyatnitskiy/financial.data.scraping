c53f77c8395816bcc76124a16a4603d9c6d5728d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-02 12:29:52 +0500

    Update CAC 40 Ticker List.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Wikipedia/CAC 40 Ticker List.R
+++ Wikipedia/CAC 40 Ticker List.R
@@ -2,20 +2,10 @@ library("rvest") # Library
 
 cac40.list.wiki <- function(x){ # Tickers from CAC 40
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # Get HTML for index data with tickers
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[5]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # Store tickers
-  
-  for (n in 0:(length(y) / 4)){ # Clean data
-    
-    v <- rbind.data.frame(v, gsub('["\n"]', '', y[4 + n * 4])) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  v # Display
+  gsub('["\n"]', '', s[seq(from = 4, to = length(s), by = 4)]) # Display
 }
 cac40.list.wiki("CAC_40") # Test
