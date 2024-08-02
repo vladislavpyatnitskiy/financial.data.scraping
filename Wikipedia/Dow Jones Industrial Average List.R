976dbbd1e709bceccca5c96d62d155d2ed08dc1d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-02 12:37:11 +0500

    Update Dow Jones Industrial Average List.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Wikipedia/Dow Jones Industrial Average List.R
+++ Wikipedia/Dow Jones Industrial Average List.R
@@ -2,20 +2,10 @@ library("rvest") # Library
 
 dowjones.list.wiki <- function(x){ # Show list of tickers from DJIA
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[2]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # Get HTML for index data with tickers
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[2]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # Store tickers
-  
-  for (n in 0:(length(y) / 6)){ # Clean data
-    
-    v <- rbind.data.frame(v, gsub('["\n"]', '', y[2 + n * 6])) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  v # Display
+  gsub('["\n"]', '', s[seq(from = 2, to = length(s), by = 6)]) # Display
 }
 dowjones.list.wiki("Dow_Jones_Industrial_Average") # Test
