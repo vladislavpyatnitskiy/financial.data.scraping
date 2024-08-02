f6d05cebcfcedd1330183156eee591b46ca98a5a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-02 12:32:44 +0500

    Update RTS Index Ticker List.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Wikipedia/RTS Index Ticker List.R
+++ Wikipedia/RTS Index Ticker List.R
@@ -2,20 +2,10 @@ library("rvest") # library
 
 rtsi.list.wiki <- function(x){ # Tickers from RTS Index
   
-  s <- read_html(paste("https://ru.wikipedia.org/wiki/", x, sep = ""))
+  s <- read_html(paste("https://ru.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # Get HTML for index data with tickers
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[5]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # Store tickers
-  
-  for (n in 0:(length(y) / 7)){ # Clean data
-    
-    v <- rbind.data.frame(v, gsub('["\n"]', '', y[2 + n * 7])) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  v # Display
+  gsub('["\n"]', '', s[seq(from = 2, to = length(s), by = 7)]) # Display
 }
 rtsi.list.wiki("Индекс_РТС") # Test
