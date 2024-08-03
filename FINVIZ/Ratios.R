07fd8dce40d90c6ab0b140e8e36024170a7c0914
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-03 11:57:36 +0500

    Update Ratios.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence
    • Reduced elapsed time from 5.023 to 3.428

--- FINVIZ/Ratios.R
+++ FINVIZ/Ratios.R
@@ -4,28 +4,22 @@ finviz.ratios <- function(x){ v <- NULL # Where to store values
 
   for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
   
-    s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j))
+    s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j)) %>%
+      html_nodes('table') %>% .[[10]] %>% html_nodes('tr') %>%
+      html_nodes('td') %>% html_text()
     
-    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab # Assign Table 
-    
-    y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-    
-    d <- NULL # Data Frame with values
-    
-    for (n in 0:(length(y) / 2)){ d <- rbind(d,cbind(y[(1+n*2)],y[(2+n*2)])) }
-    
-    d <- d[-nrow(d),] # Reduce last column
-    
+    d <- data.frame(s[seq(from=1,to=length(s),by=2)], s[seq(from=2,to=length(s),
+                                                            by=2)])
     d[27,1] <- "EPS next Y (%)" # Change name
-    
+      
     rownames(d) <- d[,1] # Assign row names
-    
+      
     d <- subset(d, select = -c(1)) # Reduce excessive column
-    
+      
     colnames(d) <- j # Assign column name
-    
+      
     if (is.null(v)){ v <- d } else { v <- cbind(v, d) } } # Join
-    
+      
   v # Display
 }
 finviz.ratios(x = c("AAPL", "AMZN", "GOOGL", "META", "MSFT")) # Test
