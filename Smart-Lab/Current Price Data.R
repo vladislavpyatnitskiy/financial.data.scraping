1b4dea6ba81335ee7414a3b47a835f7e15bb7b46
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-03 13:57:45 +0500

    Update Current Price Data.R
    Optimised script by reducing excessive lines

--- Smart-Lab/Current Price Data.R
+++ Smart-Lab/Current Price Data.R
@@ -2,16 +2,12 @@ library("rvest") # library
 
 smartlab.info <- function(x){ # Current Stock Price Data
   
-  p <- read_html(x) # Get info from website
-  
-  div <- p %>% html_nodes('table') %>% .[[1]] -> tab # Table
-  
-  f <- tab %>% html_nodes('tr') #%>% html_nodes('td') %>% html_text()
+  f <- read_html(x) %>% html_nodes('table') %>% .[[1]] %>% html_nodes('tr') 
   
   L <- NULL # Reorganise data into data frame
   
   for (n in 2:length(f)){ l <- f[n] %>% html_nodes('td') %>% html_text()
-    
+  
     L <- rbind.data.frame(L, cbind(l[3], l[7])) } # Info about ticker and price
   
   colnames(L) <- c("ticker", "price") # Column names
