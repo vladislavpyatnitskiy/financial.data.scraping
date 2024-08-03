d4122b8d2f26c7d7695756f3b66dddcced930b94
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-03 11:40:56 +0500

    Update Dividends by Sectors.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with the Sequence

--- FINVIZ/Dividends by Sectors.R
+++ FINVIZ/Dividends by Sectors.R
@@ -2,30 +2,22 @@ library("rvest") # Library
 
 finviz.sectors.dividends <- function(x){ # Dividends Yield for each sector
   
-  s <- read_html(sprintf("https://finviz.com/%s", x))
+  s <- read_html(sprintf("https://finviz.com/%s", x)) %>%
+    html_nodes('table') %>% .[[8]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # Get data
   
-  s.yahoo <- s %>% html_nodes('table') %>% .[[8]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  d <- NULL # Data Frame with values
-  
-  for (n in 0:(length(y) / 11)){ d<-rbind(d,cbind(y[(2+n*11)],y[(5+n*11)])) }
-  
-  d <- d[-nrow(d),] # Reduce last column
-  
+  d <- data.frame(s[seq(from=2,to=length(s),by=11)], s[seq(from=5,to=length(s),
+                                                          by=11)])
   rownames(d) <- d[,1] # Assign row names
   
   d <- subset(d, select = -c(1)) # Reduce excessive column
   
-  colnames(d) <- c("Dividend Yield (%)") 
+  colnames(d) <- c("Dividend Yield (%)") # Assign Column name
   
   for (n in 1:nrow(d)){ # Reduce "%" from Dividend Yield column values
     
     d[n,1] <- read.fwf(textConnection(d[n,1]), widths = c(nchar(d[n,1]) - 1, 1),
                        colClasses = "character")[,1] }
-  
-  d <- as.data.frame(d) # Transform to data frame
   
   for (n in 1:ncol(d)){ d[,n] <- as.numeric(d[,n]) } # Make data numeric
   
