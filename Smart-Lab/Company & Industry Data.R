74a0e2ca327b0e8b5b9d784e7872dd117f6787ad
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-04 10:53:13 +0500

    Update Company & Industry Data.R
    Optimised script:
    
    • Reduced excessive lines

--- Smart-Lab/Company & Industry Data.R
+++ Smart-Lab/Company & Industry Data.R
@@ -4,21 +4,17 @@ smartlab.sectors.all <- function(x){ # Data Frames with companies & industries
   
   D <- NULL # variable for data frame
   
-  for (n in 1:27){ p <- read_html(sprintf("https://smart-lab.ru/forum/%s/", x))
+  for (n in 1:27){ p <- read_html(sprintf("https://smart-lab.ru/forum/%s/",
+                                          x)) %>% html_nodes('body')
+  
+    d <- data.frame(p %>% html_nodes('ul') %>% .[[n + 1]] %>%
+                      html_nodes('li') %>% html_elements('a') %>% html_text(),
+                    p %>% html_nodes('h2') %>% .[[n]] %>% html_text() ) # Join
     
-    l <- p %>% html_nodes('body') %>% html_nodes('ul') %>% .[[n + 1]] -> tab
-    h <- p %>% html_nodes('body') %>% html_nodes('h2') %>% .[[n]] -> tab1
-    
-    h <- tab1 %>% html_text() # Subtract company names and industries
-    
-    y <- tab %>% html_nodes('li') %>% html_elements('a') %>% html_text()   
-    
-    d <- data.frame(y, h) # Join
-    
-    colnames(d) <- c("Компания", "Сектор") # Set up column names
+    colnames(d) <- c("Компания", "Сектор") # Set up Column Names
     
     D <- rbind(D, d) } # Join data of all industries
-  
+    
   D # Display
 }
 smartlab.sectors.all("sectors") # Test
