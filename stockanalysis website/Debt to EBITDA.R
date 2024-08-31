1fca0bd97a3450eb154ecf52d211a1bd233dcd0e
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-31 11:11:23 +0500

    Create Debt to EBITDA.R
    Added script getting Debt/EBITDA values for stocks from stockanalytics website

---
+++ stockanalysis website/Debt to EBITDA.R
@@ -0,0 +1,20 @@
+library("rvest") # Library
+
+debt.editda.sa <- function(x){ # Debt / EBITDA ratio
+  
+  L <- NULL
+  
+  for (n in 1:length(x)){ # Get data
+  
+    p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/statistics/",
+                           tolower(x[n]))) %>% html_nodes('table') %>%
+      html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    
+    L <- rbind.data.frame(L, as.numeric(p[grep("  Debt / EBITDA  ", p) + 1])) }
+  
+  rownames(L) <- x # tickers as row names
+  colnames(L) <- "Debt/EBITDA" # Column name
+  
+  L # Display
+}
+debt.editda.sa(c("ZIM", "AMZN")) # Test
