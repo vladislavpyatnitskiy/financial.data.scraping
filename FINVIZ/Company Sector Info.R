9341896901dc444e2706032a7ff0ab58e2e482cf
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-01 10:37:23 +0500

    Update Company Sector Info.R
    Reduced excessive lines

--- FINVIZ/Company Sector Info.R
+++ FINVIZ/Company Sector Info.R
@@ -1,17 +1,15 @@
 library("rvest") # Library
 
 finviz.sector <- function(x){ l <- NULL # Where to contain values
-  
+
   for (n in 1:length(x)){ s <- x[n] # Get sector info for each ticker
   
-    p <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", s))
-    
-    Y <- p %>% html_nodes('body') %>% .[[1]] -> tab
-    
-    y <- tab %>% html_nodes('div') %>% html_elements('a') %>% html_text()   
+    y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", s)) %>%
+      html_nodes('body') %>% .[[1]] %>% html_nodes('div') %>%
+      html_elements('a') %>% html_text()   
     
     l <- rbind(l, y[4]) } # Join
-
+    
   colnames(l) <- "Sector" # Column name for data frame
   rownames(l) <- x # tickers
   
