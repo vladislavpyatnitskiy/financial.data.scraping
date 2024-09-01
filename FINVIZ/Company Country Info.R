beac0919a2a87a2378000e5329ddafafef01942b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-01 10:39:32 +0500

    Update Company Country Info.R
    Reduced excessive lines

--- FINVIZ/Company Country Info.R
+++ FINVIZ/Company Country Info.R
@@ -1,14 +1,12 @@
 library("rvest") # Library
 
 finviz.country <- function(x){ l <- NULL # Get Country Info for each stock
-  
+
   for (n in 1:length(x)){ s <- x[n]
     
-    p <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", s))
-    
-    Y <- p %>% html_nodes('body') %>% .[[1]] -> tab
-    
-    y <- tab %>% html_nodes('div') %>% html_elements('a') %>% html_text()   
+    y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", s)) %>%
+      html_nodes('body') %>% .[[1]] %>% html_nodes('div') %>%
+      html_elements('a') %>% html_text()   
     
     l <- rbind(l, y[6]) } # Join
     
