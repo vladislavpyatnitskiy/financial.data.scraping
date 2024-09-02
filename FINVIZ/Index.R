64c5e9e3b54c476da807da06f2769c5690f334ba
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-02 11:19:09 +0500

    Update Index.R
    Reduced excessive lines

--- FINVIZ/Index.R
+++ FINVIZ/Index.R
@@ -3,15 +3,13 @@ library("rvest") # Library
 finviz.ratios.index <- function(x){ v <- NULL # Where to store values
 
   for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
-  
-    s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j))
-  
-    s.yahoo <- s %>% html_nodes('table') %>% .[[10]] -> tab # Assign Table 
-  
-    y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
+    
+    y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j)) %>%
+      html_nodes('table') %>% .[[10]] %>% html_nodes('tr') %>%
+      html_nodes('td') %>% html_text()
+    
     v <- rbind(v, y[grep("Index", y) + 1]) } # Join
-
+    
   rownames(v) <- x
   colnames(v) <- "Index"
   
