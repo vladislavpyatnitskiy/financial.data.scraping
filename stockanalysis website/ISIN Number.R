56951317c7cf5dd9054aa5387e51aa8edfe80a17
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-03 11:07:51 +0500

    Update ISIN Number.R
    Reduced excessive lines

--- stockanalysis website/ISIN Number.R
+++ stockanalysis website/ISIN Number.R
@@ -6,16 +6,14 @@ stock.analysis.isin <- function(x){ # ISIN Number of the stock
   
   for (n in 1:length(x)){ i <- x[n]
   
-    p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/company/", i))
+    p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/company/",
+                           i)) %>% html_nodes('table') %>% .[[3]] %>%
+      html_nodes('tr') %>% html_nodes('td') %>% html_text() 
     
-    price.yahoo1 <- p %>% html_nodes('table') %>% .[[3]] -> tab
+    l <- rbind(l, p[14]) } # Data Frame with ISIN Number
     
-    B <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text() 
-      
-    l <- rbind(l, B[14]) } # Data Frame with ISIN Number
-  
   rownames(l) <- x # Tickers
-  colnames(l) <- B[13] # ISIN Number column name
+  colnames(l) <- "ISIN" # ISIN
   
   l # Display
 }
