bddd6fc0a3fface71e48c7377dfe2c8ec104b412
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-24 11:13:06 +0500

    Update Daily data.R
    Optimised script:
    
    • Put Data Frame Joiner from IF statements
    • Reduced system time parameters at least by 50%
    • Reduced excessive lines
    • Edited comments

--- Yahoo/Price data/Daily data.R
+++ Yahoo/Price data/Daily data.R
@@ -2,23 +2,19 @@ lapply(c("quantmod", "timeSeries"), require, character.only = T) # libraries
 
 prices.yahoo <- function(y, s = NULL, e = NULL){ # Stock Price Data from Yahoo
   
-  p <- NULL # There are 4 conditions for getting data:
+  p <- NULL # 4 scenarios: no dates, only start or end dates, both dates
   
-  for (A in y){ if (is.null(s) && is.null(e)) { # When Time Period not defined
+  for (A in y){ if (is.null(s) && is.null(e)) { 
     
-      p <- cbind(p, getSymbols(A, src = "yahoo", auto.assign = F)[,4])
+      q <- getSymbols(A, src = "yahoo", auto.assign = F)
     
-    } else if (is.null(e)) { # When only start date is defined
+    } else if (is.null(e)){ q <- getSymbols(A,from=s,src="yahoo",auto.assign=F)
+  
+    } else if (is.null(s)){ q <- getSymbols(A, to=e, src="yahoo",auto.assign=F)
+  
+    } else { q <- getSymbols(A, from = s, to = e, src="yahoo", auto.assign=F) }
     
-      p <- cbind(p,getSymbols(A, from = s, src = "yahoo", auto.assign = F)[,4])
-    
-    } else if (is.null(s)) { # When only end date is defined
-      
-      p <- cbind(p, getSymbols(A, to = e, src = "yahoo", auto.assign = F)[,4])
-    
-    } else { # When both start date and end date are defined
-      
-      p <- cbind(p,getSymbols(A,from=s,to=e,src="yahoo",auto.assign=F)[,4]) } }
+    p <- cbind(p, q[,4]) } # Join all columns into one data frame
   
   p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
@@ -26,4 +22,4 @@ prices.yahoo <- function(y, s = NULL, e = NULL){ # Stock Price Data from Yahoo
   
   as.timeSeries(p) # Make it time series and display
 }
-prices.yahoo(y = "UNM", s = "2022-01-01") # Test
+prices.yahoo(y = c("UNM", "AIG", "HIG"), s = "2022-01-01") # Test
