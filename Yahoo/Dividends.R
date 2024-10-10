8ad9f5a2b02656b298116bfdcc94a02aa6926ddd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-10 11:29:19 +0500

    Update Dividends.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted extended IF statements with nested function

--- Yahoo/Dividends.R
+++ Yahoo/Dividends.R
@@ -1,118 +1,58 @@
 lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libs
 
-s.dividends <- function(x, s = NULL, e = NULL){ # Dividends data
+s.dividends <- function(x, s = NULL, e = NULL) { # Get Dividends data
   
-  d <- NULL # Create an empty variables
-  no.d <- NULL # list of companies not paying dividends for the period
+  d <- NULL # For stocks paying dividends
+  no.d <- NULL # For stocks not paying dividends
   
-  for (A in x){ if (is.null(s) && is.null(e)) { # When no start or end date
+  getDivData <- function(A, s, e) {
+    if (is.null(s) && is.null(e)) return(getDividends(A)) # No period defined
+    if (is.null(e)) return(getDividends(A, from = s)) # Start Date is defined
+    if (is.null(s)) return(getDividends(A, to = e)) # End Date is defined
+    return(getDividends(A, from = s, to = e)) # Start and End Date
+  }
+  
+  for (A in x) {
+    d2 <- getDivData(A, s, e) # Get data
     
-    if (is.null(d)){ d <- getDividends(A) # Get dividends or add to new list
-    
-    if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
+    if (isTRUE(length(d2) == 0)) { # if stock no paying dividend, add to list
+      no.d <- c(no.d, A)
+    } else {
+      r.names2 <- rownames(d2) # otherwise, prepare data frame to merge
+      d2 <- cbind(r.names2, d2)
       
-      r.names <- rownames(d) # Subtract dates
+      if (is.null(d)) {
+        d <- d2
+      } else {
+        d <- merge(d, d2, all = T) # Merge by dates
+      }
+    }
+  }
+  
+  if (!is.null(d)) { # if there are dividend paying companies, 
+    d[is.na(d)] <- 0 # Assign Not available values as 0
+    d <- as.timeSeries(d)
+    k <- colnames(d)
+    
+    if (length(no.d) > 0) { # to merge not paying companies with others
+      r.names3 <- as.data.frame(rownames(d))
+      r.names5 <- NULL
       
-      d <- cbind(r.names, d) } } else { d2 <- getDividends(A)
-      
-      if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
-        
-        r.names2 <- rownames(d2) # Subtract dates
-        
-        d2 <- cbind(r.names2, d2) # Join
-        
-        d <- merge(d, d2, all = T) } } # When there is start date:
-    
-  } else if (is.null(e)) { if (is.null(d)){ d <- getDividends(A, from = s)
-  
-  if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
-    
-    r.names <- rownames(d) # Subtract dates
-    
-    d <- cbind(r.names, d) } } else { d2 <- getDividends(A, from = s)
-    
-    if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
-      
-      r.names2 <- rownames(d2) # Subtract dates
-      
-      d2 <- cbind(r.names2, d2) # Join
-      
-      d <- merge(d, d2, all = T) } } # When there is end date:
-    
-  } else if (is.null(s)) { if (is.null(d)){ d <- getDividends(A, to = e)
-  
-  if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
-    
-    r.names <- rownames(d) # Subtract dates
-    
-    d <- cbind(r.names, d) } } else { d2 <- getDividends(A, to = e)
-    
-    if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
-      
-      r.names2 <- rownames(d2) # Subtract dates
-      
-      d2 <- cbind(r.names2, d2) # Join
-      
-      d<-merge(d, d2, all=T) } } # When there are both start and end dates:
-    
-  } else { if (is.null(d)){ d <- getDividends(A, from = s, to = e)
-  
-  if (isTRUE(length(d) == 0)){ no.d <- c(no.d, A) } else {
-    
-    r.names <- rownames(d) # Subtract dates
-    
-    d <- cbind(r.names, d) } } else { d2 <- getDividends(A, from = s, to = e)
-    
-    if (isTRUE(length(d2) == 0)){ no.d <- c(no.d, A) } else {
-      
-      r.names2 <- rownames(d2) # Subtract dates
-      
-      d2 <- cbind(r.names2, d2) # Join
-      
-      d <- merge(d, d2, all = T) } } } } # End of data collection
-  
-  d[is.na(d)] <- 0 # Substitute NA with 0
-  
-  k <- colnames(d)
-  
-  d <- as.timeSeries(d) # Make them Time Series
-  
-  r.names3 <- as.data.frame(rownames(d)) # Transform data into Data Frame
-  
-  r.names5 <- NULL # Create Data Frame for Stocks without dividends
-  
-  for (n in 1:length(no.d)){ r.names4 <- data.frame(r.names3, 0)
-  
-    colnames(r.names4) <- c("Date", no.d[n])
-    
-    if (is.null(r.names5)){ r.names5 <- r.names4 } else {
-      
-      r.names5 <- merge(r.names5, r.names4, by = "Date") } }
-    
-  rownames(r.names5) <- r.names5[,1] # Put dates into row names
-  
-  r.names5 <- r.names5[,-1] # Reduce dates column
-  
-  d <- cbind(d, as.timeSeries(r.names5)) # Join
-  
-  k <- c(k, no.d)
-  
-  colnames(d) <- k
-  
-  l <- NULL # Create empty list to clean column names
-  
-  for (n in 1:length(colnames(d))){
-    
-    if (isTRUE(grepl("div", colnames(d)[n], fixed = T))){ 
-      
-      l <- c(l, gsub('[".div"]','', colnames(d)[n]))
-      
-      } else { l <- c(l, colnames(d)[n]) } } #
-  
-  colnames(d) <- l # Put clean names to Data Frame
-  
-  d <- d[,x] # Change order of columns
-  
-  d # Display
+      for (n in no.d) {
+        r.names4 <- data.frame(r.names3, 0)
+        colnames(r.names4) <- c("Date", n)
+        r.names5 <- if (is.null(r.names5)) r.names4 else merge(r.names5,
+                                                               r.names4,
+                                                               by = "Date")
+      }
+      rownames(r.names5) <- r.names5[, 1] # Put dates in row names 
+      r.names5 <- r.names5[, -1] # Reduce date values from main data frame
+      d <- cbind(d, as.timeSeries(r.names5)) # Make it in time series type
+      k <- c(k, no.d) # Put ticker names by order it got while extracting 
+    }
+    colnames(d) <- gsub('[".div"]', '', k) # Reduce ".div" from ticker names
+    d <- d[, x] # Put at the same order as typed in function
+  }
+  return(d) # display
 }
-s.dividends(c("AAPL","MSFT","AMZN","C","GOOGL","VSTO"),s="2022-07-19") # Test
+s.dividends(c("AAPL","MSFT","AMZN","C","GOOGL","VSTO"), s="2022-07-19") # Test
