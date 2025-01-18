f3c28a41ad723eed71a7e8ce45453050c8d5ad38
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-18 21:53:13 +0500

    Update Ratings.R
    Rewrote HTML nodes

--- stockanalysis website/Ratings.R
+++ stockanalysis website/Ratings.R
@@ -5,12 +5,10 @@ rating.sa <- function(x){ # Company Ratings
   D <- NULL
   
   for (n in 1:length(x)){ 
-  
+    
     p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/ratings/",
-                           tolower(x[n])))
-  
-    p <- p %>% html_nodes('main') %>% html_nodes('div') %>%
-      html_nodes('div') %>% html_text()
+                           tolower(x[n]))) %>% html_nodes('main') %>%
+      html_nodes('div') %>% html_nodes('div') %>% html_text()
     
     f <- unlist(strsplit(p[grepl("Price Target", p)][1], " "))
     f <- f[length(f)]
@@ -19,7 +17,7 @@ rating.sa <- function(x){ # Company Ratings
     u <- u[length(u)]
     
     D <- rbind.data.frame(D, cbind(p[9], f, u)) } # Merge values
-    
+  
   colnames(D) <- c("Current Price", "Predicted Price", "Upside") # Column names
   rownames(D) <- x # Tickers
   
