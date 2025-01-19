ef3d7c6136fbe526ddeb69c326ec4a48fffbee8d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-01-19 12:00:39 +0500

    Update Ratings.R
    Rewrote part for joining info of current price and upside

--- stockanalysis website/Ratings.R
+++ stockanalysis website/Ratings.R
@@ -4,23 +4,23 @@ rating.sa <- function(x){ # Company Ratings
   
   D <- NULL
   
-  for (n in 1:length(x)){ 
+  for (n in 1:length(x)){ l <- c("Price Target", "Upside")
     
     p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/ratings/",
                            tolower(x[n]))) %>% html_nodes('main') %>%
       html_nodes('div') %>% html_nodes('div') %>% html_text()
     
-    f <- unlist(strsplit(p[grepl("Price Target", p)][1], " "))
-    f <- f[length(f)]
+    d <- NULL # Info with Predicted Price and Upside
     
-    u <- unlist(strsplit(p[grepl("Upside", p)][1], " "))
-    u <- u[length(u)]
+    for (m in 1:2){ f <- unlist(strsplit(p[grepl(l[m], p)][1], " "))
     
-    D <- rbind.data.frame(D, cbind(p[9], f, u)) } # Merge values
-  
+      d <- c(d, f[length(f)]) } # Join into vector
+    
+    D <- rbind.data.frame(D, cbind(p[9], d[1], d[2])) } # Merge values
+    
   colnames(D) <- c("Current Price", "Predicted Price", "Upside") # Column names
   rownames(D) <- x # Tickers
   
   D # Display
 }
-rating.sa(c("VIRT")) # Test
+rating.sa(c("VIRT", "C")) # Test
