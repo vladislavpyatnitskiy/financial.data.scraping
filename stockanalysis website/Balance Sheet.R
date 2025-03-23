32513bc5a3f35189af1cc7b6cdbba718970d65ef
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-03-23 15:23:43 +0500

    Update Balance Sheet.R
    Optimised script:
    
    • Fixed issue with a wrong alignment due to changes of website structure 
    • Reduced excessive lines
    • Added option to sort years (ascendingly and descendingly)

--- stockanalysis website/Balance Sheet.R
+++ stockanalysis website/Balance Sheet.R
@@ -1,27 +1,25 @@
 library(rvest) # Library
 
-balance.sheet.sa <- function(x){ # Balance Sheet from stockanalytics
-    
+balance.sheet.sa <- function(x, sort = T){ # Balance Sheet 
+  
   p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/financials/%s/",
                          tolower(x), "balance-sheet")) %>%
-    html_nodes('table') %>% html_nodes('tr')
+    html_nodes('body') %>% html_nodes('table') %>% html_nodes('tr') 
   
   R <- p %>% html_nodes('td') %>% html_text() # Main data
-  
   C <- p %>% html_nodes('th') %>% html_text() # Titles (Column names)
   
-  C <- gsub('["\n"]', '', gsub('["\t"]', '', C[2:7])) # Clean
+  C <- unlist(strsplit(gsub('["\n"]', '', gsub('["\t"]', '', C[2:6])), " "))
+
+  D <- data.frame(R[seq(from = 2, to = length(R), by = 7)], # Previous Year
+                  R[seq(from = 3, to = length(R), by = 7)], # -1
+                  R[seq(from = 4, to = length(R), by = 7)], # -2
+                  R[seq(from = 5, to = length(R), by = 7)], # -3
+                  R[seq(from = 6, to = length(R), by = 7)]) # -4
   
-  D <- data.frame(R[seq(from = 2, to = length(R), by = 8)], # TTM
-                  R[seq(from = 3, to = length(R), by = 8)], # 2023
-                  R[seq(from = 4, to = length(R), by = 8)], # 2022
-                  R[seq(from = 5, to = length(R), by = 8)], # 2021
-                  R[seq(from = 6, to = length(R), by = 8)], # 2020
-                  R[seq(from = 7, to = length(R), by = 8)]) # 2019
+  rownames(D) <- R[seq(from = 1, to = length(R), by = 7)] # Row names
+  colnames(D) <- C[seq(from = 2, to = length(C), by = 2)] # Column names
   
-  rownames(D) <- R[seq(from = 1, to = length(R), by = 8)] # Row names
-  colnames(D) <- C # Column names
-  
-  D # Display
+  if (isTRUE(sort)){ D[,sort(colnames(D))] } else { D } # Display
 }
-balance.sheet.sa("ZIM") # Test
+balance.sheet.sa("ZIM", F) # Test
