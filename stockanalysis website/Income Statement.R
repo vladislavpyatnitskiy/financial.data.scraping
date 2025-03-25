d95d5fb94437f42fdce55121a62044c53cc7e1e8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-03-25 11:14:43 +0500

    Update Income Statement.R
    Updated script:
    
    • Set up for new web site alignment
    • Added option to sort years

--- stockanalysis website/Income Statement.R
+++ stockanalysis website/Income Statement.R
@@ -1,27 +1,25 @@
 library(rvest) # Library
 
-income.statement.sa <- function(x){ # Income Statement from stockanalytics
-    
+income.statement.sa <- function(x, sort = T){ # Income Statement 
+  
   p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/financials/",
                          tolower(x))) %>% html_nodes('table') %>%
     html_nodes('tr')
   
   R <- p %>% html_nodes('td') %>% html_text() # Main data
-  
   C <- p %>% html_nodes('th') %>% html_text() # Titles (Column names)
   
-  C <- gsub('["\n"]', '', gsub('["\t"]', '', C[2:7])) # Clean
+  C <- unlist(strsplit(gsub('["\n"]', '', gsub('["\t"]', '', C[2:6])), " "))
   
-  D <- data.frame(R[seq(from = 2, to = length(R), by = 8)], # TTM
-                  R[seq(from = 3, to = length(R), by = 8)], # 2023
-                  R[seq(from = 4, to = length(R), by = 8)], # 2022
-                  R[seq(from = 5, to = length(R), by = 8)], # 2021
-                  R[seq(from = 6, to = length(R), by = 8)], # 2020
-                  R[seq(from = 7, to = length(R), by = 8)]) # 2019
+  D <- data.frame(R[seq(from = 2, to = length(R), by = 7)], # Previous Year
+                  R[seq(from = 3, to = length(R), by = 7)], # -1
+                  R[seq(from = 4, to = length(R), by = 7)], # -2
+                  R[seq(from = 5, to = length(R), by = 7)], # -3
+                  R[seq(from = 6, to = length(R), by = 7)]) # -4
   
-  rownames(D) <- R[seq(from = 1, to = length(R), by = 8)] # Row names
-  colnames(D) <- C # Column names
+  rownames(D) <- R[seq(from = 1, to = length(R), by = 7)] # Row names
+  colnames(D) <- C[seq(from = 2, to = length(C), by = 2)] # Column names
   
-  D # Display
+  if (isTRUE(sort)){ D[,sort(colnames(D))] } else { D } # Display
 }
 income.statement.sa("ZIM") # Test
