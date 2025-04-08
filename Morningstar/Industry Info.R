4c5a273c28fea8e1103bdbcbe5b7f6daf7faf2aa
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-08 10:23:41 +0500

    Update Industry Info.R
    Revived script:
    
    • Added new libraries making script working again

--- Morningstar/Industry Info.R
+++ Morningstar/Industry Info.R
@@ -1,14 +1,20 @@
-library("rvest") # Library
+lapply(c("rvest", "httr", "xml2"), require, character.only = T) # Libs
 
-morningstar.industry <- function(x){ # Get Industry Info from Morningstar
+morningstar.industry <- function(x){ # Get Sector Info from Morningstar
   
   L <- NULL 
   
   for (m in 1:length(x)){ s <- tolower(x[m]) # Get HTML and clean it
-  
-    f <- read_html(sprintf("https://www.morningstar.com/stocks/misx/%s/quote",
-                           s)) %>% html_nodes('section') %>%
-      html_nodes('dd') %>% html_nodes('span') 
+    
+    url <- sprintf("https://www.morningstar.com/stocks/misx/%s/quote", s)
+    
+    B <- paste("Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+               "AppleWebKit/537.36", "Chrome/122.0.0.0", "Safari/537.36",
+               sep = " ")
+    
+    f <- read_html(content(GET(url, add_headers(`User-Agent` = B)),
+                           as = "text", encoding = "UTF-8")) %>%
+      html_nodes('section') %>% html_nodes('dd') %>% html_nodes('span') 
     
     l <- NULL 
     
@@ -18,7 +24,7 @@ morningstar.industry <- function(x){ # Get Industry Info from Morningstar
       l <- c(l, f[n] %>% html_text()) } } # Final version
     
     L <- rbind.data.frame(L, l[2]) } # Join
-    
+  
   colnames(L) <- "Industry" # Assign Column name
   rownames(L) <- x # assign tickers as row names
   
