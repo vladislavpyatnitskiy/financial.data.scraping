57ad042552fc50d3efc3a815aeb1df3e48d4f516
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-10 10:09:43 +0500

    Update Company Industry Info.R
    Revived script by adding more libraries for web scraping

--- Yahoo/Company Industry Info.R
+++ Yahoo/Company Industry Info.R
@@ -1,20 +1,25 @@
-library("rvest") # Library
+lapply(c("rvest", "httr", "xml2"), require, character.only = T) # Libs
 
-c.industry <- function(x){ l <- NULL # Create list
-
-  for (n in 1:length(x)){ s <- x[n] # For each security find industry
-    
-    p <- read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/profile", s))
-    
-    Y <- p %>% html_nodes('div') %>% .[[1]] -> tab
-    
-    y <- tab %>% html_nodes('p') %>% html_nodes('span') %>% html_text()
-    
-    l <- rbind(l, y[grep("Industry", y) + 1]) } # Add to list
-    
-  colnames(l) <- "Industry" # 
-  rownames(l) <- x #
+c.industry <- function(x){ L <- NULL #l <- NULL # Create list
   
-  l # Display
+  for (n in 1:length(x)){ s <- x[n]
+    
+    B <- paste("Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+               "AppleWebKit/537.36", "Chrome/122.0.0.0", "Safari/537.36",
+               sep = " ")
+    
+    response <- GET(sprintf("https://uk.finance.yahoo.com/quote/%s/profile", s),
+                    add_headers(`User-Agent` = B))
+    
+    f <- read_html(content(response, as = "text", encoding = "UTF-8")) %>%
+      html_nodes('div') %>% html_nodes('dl') %>% html_nodes('div') %>%
+      html_nodes('strong') %>% html_text() %>% .[2]
+      
+    L <- rbind.data.frame(L, f) }
+  
+  colnames(L) <- "Industry" # 
+  rownames(L) <- x #
+  
+  L # Display
 }
 c.industry(x = c("AAPL", "NRG", "PVH", "C")) # Test
