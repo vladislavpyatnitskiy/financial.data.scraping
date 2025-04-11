23e769850a716c953320aadd6adae4ac67c652cb
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-11 11:30:09 +0500

    Update Company Country Info.R
    Revived script:
    
    • Added new libraries (httr and xml2)
    • Rewrote part for html nodes

--- Yahoo/Company Country Info.R
+++ Yahoo/Company Country Info.R
@@ -1,23 +1,29 @@
-library("rvest") # Library
+lapply(c("rvest", "httr", "xml2"), require, character.only = T) # Libs
 
-c.country <- function(x){ l <- NULL # Create list
-
-  for (n in 1:length(x)){ s <- x[n] # For each security find sector
+c.country <- function(x){ # Get country info for securities
   
-    p <- sprintf("https://uk.finance.yahoo.com/quote/%s/profile?p=%s", s, s)
+  l <- NULL
+  
+  for (n in 1:length(x)){ s <- x[n]
     
-    page.p <- read_html(p) # Read HTML & extract necessary info
+    B <- paste("Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+               "AppleWebKit/537.36", "Chrome/122.0.0.0", "Safari/537.36",
+               sep = " ")
     
-    price.yahoo1 <- page.p %>% html_nodes('div') %>% .[[1]] -> tab
+    response <- GET(sprintf("https://uk.finance.yahoo.com/quote/%s/profile/",
+                            s), add_headers(`User-Agent` = B))
+  
+    y <- read_html(response) %>% html_nodes('main') %>%
+      html_nodes('section') %>% html_nodes('section') %>%
+      html_nodes('article') %>% html_nodes('section') %>%
+      html_nodes('section') %>% html_nodes('div') %>% html_nodes('div') %>%
+      html_text()
     
-    y <- tab %>% html_nodes('p') # Find country character in elements
+    l <- rbind(l, y[grep("Sector", y) - 1]) } # Join 
     
-    l <- rbind(l, strsplit(toString(y),
-                           "<br>")[[1]][length(strsplit(toString(y),
-                                                        "<br>")[[1]])-4]) }
   colnames(l) <- "Country"
   rownames(l) <- x
-  
+
   l # Display
 }
-c.country(c("STLA", "ZIM", "AMZN", "VALE")) # Test
+c.country(c("AMZN", "VALE", "STLA", "ZIM")) # Test
