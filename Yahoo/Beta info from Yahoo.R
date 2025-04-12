573722b2c000939547768c391ba3097188d1cdbd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-12 12:22:34 +0500

    Update Beta info from Yahoo.R
    Revived script:
    
    • Added new libraries (httr, xml2)

--- Yahoo/Beta info from Yahoo.R
+++ Yahoo/Beta info from Yahoo.R
@@ -1,4 +1,5 @@
-lapply(c("quantmod","timeSeries","rvest"),require,character.only=T) # Libraries 
+lapply(c("quantmod", "timeSeries", "rvest", "httr", "xml2"), require,
+       character.only = T) # Libraries 
 
 beta.yahoo <- function(x){ # Function to get info about company beta
   
@@ -6,17 +7,20 @@ beta.yahoo <- function(x){ # Function to get info about company beta
   
   for (n in 1:length(x)){ v <- x[n] # For every ticker get beta value
   
-    p<-read_html(sprintf("https://uk.finance.yahoo.com/quote/%s/key-statistics",
-                         v))
+    B <- paste("Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+               "AppleWebKit/537.36", "Chrome/122.0.0.0", "Safari/537.36",
+               sep = " ")
     
-    tab <- p %>% html_nodes('div') %>% .[[1]]
+    response <- GET(sprintf("https://uk.finance.yahoo.com/quote/%s/%s/", v,
+                            "key-statistics"), add_headers(`User-Agent` = B))
     
-    i <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+    y <- read_html(response) %>% html_nodes('table') %>% .[[8]] %>%
+      html_nodes('tr') %>% html_nodes('td') %>% html_text() 
     
-    b <- as.numeric(i[grep("Beta ", i) + 1]) # Select Beta value
+    b <- as.numeric(y[grep("Beta ", y) + 1]) # Select Beta value
     
     if (is.na(b)){ l <- c(v, "^GSPC") # When Beta is not available
-    
+      
       b <- NULL # Get Stock Price Data and calculate Beta yourself
       
       for (m in l){ b <- cbind(b, getSymbols(m, from=as.Date(Sys.Date())-365*5,
