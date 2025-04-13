16c92327611fabbdc7e6e6de13fb3a8756c95857
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-13 09:15:44 +0500

    Create Beta.R
    Added script getting data of beta coefficients for Russian stocks

---
+++ MOEX data/Beta.R
@@ -0,0 +1,34 @@
+lapply(c("moexer", "timeSeries", "xts"), require, character.only = T) # Libs 
+
+beta.moex <- function(y){ # Beta values for Russian stocks
+  
+  y <- c(y, "IMOEX") # Add benchmark to list
+    
+  p <- NULL # Empty variables for security values & Data upload
+  
+  start_date <- as.Date(Sys.Date() - 365 * 5) # Start Date
+  
+  for (A in y){ D <- as.data.frame(get_candles(A, start_date,
+                                               till = as.Date(Sys.Date()),
+                                               interval = 'daily')[,c(3,8)])
+  
+    D <- D[!duplicated(D),] # Remove duplicates
+    
+    p <- cbind(p, xts(D[, 1], order.by = as.Date(D[, 2]))) }
+  
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+  
+  colnames(p) <- y # Put the tickers in data set
+    
+  p <- diff(log(as.timeSeries(p)))[-1,] # Time Series Returns and NA off
+    
+  B <- apply(p[, -which(names(p) == "IMOEX")], 2,
+             function(col) ((lm((col) ~ p[,"IMOEX"]))$coefficients[2]))
+  
+  B <- as.data.frame(round(B, 2)) # Round by 2 decimal numbers
+  
+  colnames(B) <- "Beta" # Column name
+  
+  return(B) # Display values
+}
+beta.moex(c("LKOH", "RASP")) # Test
