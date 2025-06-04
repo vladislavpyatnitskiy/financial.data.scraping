dcc830f6f85d2dbcad32e8942186714deb29b374
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-04 10:42:39 +0500

    Update Industry Info.R
    Rewrote part for HTML nodes

--- stockanalysis website/Industry Info.R
+++ stockanalysis website/Industry Info.R
@@ -5,14 +5,12 @@ sa.industry <- function(x){ # Sector data from stockanalytics website
   l <- NULL
   
   for (n in 1:length(x)){ y <- x[n] # Get data
-    
+  
     p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/",
                            tolower(y))) %>% html_nodes('body') %>%
       html_nodes('main') %>% html_nodes('div') %>% html_nodes('a') 
     
-    q <- p %>% html_attr('class') == "dothref text-default" # Clean data & Join 
-    
-    l <- rbind.data.frame(l, p[grep("TRUE", q)] %>% html_text() %>% .[1]) } 
+    l <- rbind.data.frame(l, p[grep("industry", p)] %>% html_text()) } 
     
   rownames(l) <- x # tickers
   colnames(l) <- "Industry" # Column name
