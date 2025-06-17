ec5d9ca606facf8f0bd298a0fecfa3e8ed24a5fd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-06-17 13:17:44 +0500

    Update SmartLab Fundamentals.R
    Rewrote HTML nodes

--- Smart-Lab/SmartLab Fundamentals.R
+++ Smart-Lab/SmartLab Fundamentals.R
@@ -5,29 +5,27 @@ smartlab.ticker <- function(x, y){ # Fundamental
   l <- NULL # Store data here
   
   for (j in 1:length(x)){ k <- NULL
-  
+    
     for (m in 1:length(y)){ v <- y[m] # Fundamental
-    
-    s <- read_html(sprintf("https://smart-lab.ru/q/%s/?field=%s",
-                           "shares_fundamental", v))
-    
-    s.yahoo <- s %>% html_nodes('table') %>% .[[1]] -> tab
-    
-    i <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-    
-    D <- NULL
-    
-    for (n in 0:(length(i)/6)){ D <- rbind(D, cbind(i[(3+n*6)], i[(6+n*6)])) }
-    
-    D <- cbind(x[j], D[D[,1] == x[j], 2]) 
-    
-    D[,2] <- gsub('["\n"]', '', gsub('["\t"]', '', D[,2])) # Clean
-    
-    if (isTRUE(grepl(" ", D[,2]))){ D[,2] <- gsub(" ", "", D[,2]) }  
-    
-    colnames(D) <- c("Ticker", gsub("_", "/", toupper(y[m]))) # Column names
-    
-    if (is.null(k)){ k<-D } else { k<-merge(x=k,y=D,by="Ticker",all=T) } }
+      
+      i <- read_html(sprintf("https://smart-lab.ru/q/%s/?field=%s",
+                             "shares_fundamental", v)) %>%
+        html_nodes('table') %>% .[[1]] %>% html_nodes('tr') %>%
+        html_nodes('td') %>% html_text()
+      
+      D <- NULL
+      
+      for (n in 0:(length(i)/6)){ D = rbind(D, cbind(i[(3+n*6)], i[(6+n*6)])) }
+      
+      D <- cbind(x[j], D[D[,1] == x[j], 2]) 
+      
+      D[,2] <- gsub('["\n"]', '', gsub('["\t"]', '', D[,2])) # Clean
+      
+      if (isTRUE(grepl(" ", D[,2]))){ D[,2] <- gsub(" ", "", D[,2]) }  
+      
+      colnames(D) <- c("Ticker", gsub("_", "/", toupper(y[m]))) # Column names
+      
+      if (is.null(k)){ k = D } else { k = merge(x=k,y=D,by="Ticker",all=T) } }
     
     if (is.null(l)){ l <- k } else { l <- rbind(l, k) } }   # Join
   
