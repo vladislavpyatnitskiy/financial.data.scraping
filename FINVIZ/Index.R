e85bd07c69bc596679570d939c47b631cdceb720
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-12-25 10:04:05 +0500

    Revise HTML parsing logic in FINVIZ/Index.R
    Refactor HTML parsing to extract links and update message format.

--- FINVIZ/Index.R
+++ FINVIZ/Index.R
@@ -5,10 +5,17 @@ finviz.ratios.index <- function(x){ v <- NULL # Where to store values
   for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
     
     y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j)) %>%
-      html_nodes('table') %>% .[[10]] %>% html_nodes('tr') %>%
-      html_nodes('td') %>% html_text()
+      html_nodes('body') %>% .[[1]] %>% html_nodes('div') %>% 
+      html_elements('a') %>% html_text() 
+      
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        j, which(x == j), length(x)
+      )
+    )
     
-    v <- rbind(v, y[grep("Index", y) + 1]) } # Join
+    v <- rbind(v, y[7]) } # Join
     
   rownames(v) <- x
   colnames(v) <- "Index"
