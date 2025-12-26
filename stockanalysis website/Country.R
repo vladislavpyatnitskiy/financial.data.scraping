fc2741fa006f03e15f9e21f0903d14a8b3da55d8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-12-26 11:48:40 +0500

    Update Country.R
    Added download message and refactor html nodes

--- stockanalysis website/Country.R
+++ stockanalysis website/Country.R
@@ -5,13 +5,20 @@ stock.analysis.country <- function(x){ # Countries for stock
   l <- NULL # Store countries for each security
   
   for (n in 1:length(x)){ i <- x[n] # Get Country for each security
-  
-    C<-read_html(sprintf("https://stockanalysis.com/stocks/%s/company/",i)) %>%
-      html_nodes('table') %>% .[[1]] %>% html_nodes('tr') %>%
-      html_nodes('td') %>% html_text() 
+    
+    C = read_html(sprintf("https://stockanalysis.com/stocks/%s/company/",
+                          i)) %>% html_nodes('table') %>% .[[1]] %>% 
+      html_nodes('tr') %>% html_nodes('td') %>% html_text() 
+    
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        i, which(x == i), length(x)
+      )
+    )
     
     l <- rbind.data.frame(l, C[grep("Country", C) + 1]) } # Data Frame 
-  
+    
   rownames(l) <- x # Tickers
   colnames(l) <- "Country" # Country column name
   
