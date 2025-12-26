9046eee288bb0cc98cb559b899ad895b13265bc6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-12-26 11:43:15 +0500

    Enhance finviz.exchange with progress messages
    Updated finviz.exchange function to include messages during data retrieval.

--- FINVIZ/Stock Exchange Info.R
+++ FINVIZ/Stock Exchange Info.R
@@ -3,12 +3,19 @@ library("rvest") # Library
 finviz.exchange <- function(x){ l <- NULL # Info about company's stock exchange
 
   for (n in 1:length(x)){ s <- x[n] # Get info for each stock
-  
-    y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", s)) %>%
-      html_nodes('body') %>% .[[1]]  %>% html_nodes('div') %>%
-      html_elements('a') %>% html_text()   
     
-    l <- rbind(l, y[7]) } # Join
+    y <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", s)) %>%
+      html_nodes('body') %>% .[[1]] %>% html_nodes('div') %>%
+      html_elements('a') %>% html_text() %>% .[7]   
+    
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        s, which(x == s), length(x)
+      )
+    )
+    
+    l <- rbind(l, y) } # Join
     
   colnames(l) <- "Stock Exchange" # Column Name
   rownames(l) <- x # Tickers
