0092b2d18b3528b210f93268a675bcf5edc35fdd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-12-30 11:59:57 +0500

    Update Balance Sheet.R

--- stockanalysis website/Balance Sheet.R
+++ stockanalysis website/Balance Sheet.R
@@ -9,17 +9,19 @@ balance.sheet.sa <- function(x, sort = T){ # Balance Sheet
   R <- p %>% html_nodes('td') %>% html_text() # Main data
   C <- p %>% html_nodes('th') %>% html_text() # Titles (Column names)
   
-  C <- unlist(strsplit(gsub('["\n"]', '', gsub('["\t"]', '', C[2:6])), " "))
-
-  D <- data.frame(R[seq(from = 2, to = length(R), by = 7)], # Previous Year
-                  R[seq(from = 3, to = length(R), by = 7)], # -1
-                  R[seq(from = 4, to = length(R), by = 7)], # -2
-                  R[seq(from = 5, to = length(R), by = 7)], # -3
-                  R[seq(from = 6, to = length(R), by = 7)]) # -4
+  C <- unlist(strsplit(gsub('["\n"]', '', gsub('["\t"]', '', C[1:7])), " "))
+  
+  D <- data.frame(
+    R[seq(from = 2, to = length(R), by = 7)], # Previous Year
+    R[seq(from = 3, to = length(R), by = 7)], # -1
+    R[seq(from = 4, to = length(R), by = 7)], # -2
+    R[seq(from = 5, to = length(R), by = 7)], # -3
+    R[seq(from = 6, to = length(R), by = 7)] # -4
+    )
   
   rownames(D) <- R[seq(from = 1, to = length(R), by = 7)] # Row names
-  colnames(D) <- C[seq(from = 2, to = length(C), by = 2)] # Column names
+  colnames(D) <- C[grep("FY", C) + 1] # Column names
   
-  if (isTRUE(sort)){ D[,sort(colnames(D))] } else { D } # Display
+  if (sort){ D[,sort(colnames(D))] } else { D } # Display
 }
-balance.sheet.sa("ZIM", F) # Test
+balance.sheet.sa("AAPL", F) # Test
