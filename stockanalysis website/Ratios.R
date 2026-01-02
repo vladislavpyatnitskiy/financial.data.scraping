14c51f060411be32b93f8174f2746ff82e48eef3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-02 13:57:41 +0500

    Update Ratios.R
    added message download

--- stockanalysis website/Ratios.R
+++ stockanalysis website/Ratios.R
@@ -9,6 +9,13 @@ ratios.sa <- function(x){ # Ratios data from stockanalysis website
     p <- read_html(sprintf("https://stockanalysis.com/stocks/%s/statistics/",
                            tolower(x[n]))) %>% html_nodes('table') %>%
       html_nodes('tr') %>% html_nodes('td') %>% html_text()
+
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        x[n], which(x == x[n]), length(x)
+      )
+    )
     
     R <- p[seq(from = 1, to = length(p), by = 2)] # row names
     C <- as.data.frame(p[seq(from = 2, to = length(p), by = 2)]) # stock data
