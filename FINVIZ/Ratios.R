e09b6d82633f82202fe4ad49d7c572eb1c8f8e7d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-04 14:19:45 +0500

    Update Ratios.R
    Rewrote and revived script
    
    • Added download message
    • Rewrote parts for HTML nodes reading

--- FINVIZ/Ratios.R
+++ FINVIZ/Ratios.R
@@ -1,25 +1,41 @@
 library("rvest") # Library
 
-finviz.ratios <- function(x){ v <- NULL # Where to store values
-
-  for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
+finviz.ratios <- function(x){ # Get info about financial ratios for stocks
   
+  v <- NULL # Where to store values
+  
+  for (n in 1:length(x)){ j <- x[n] # Assign security and read HTML of page
+        
     s <- read_html(sprintf("https://finviz.com/quote.ashx?t=%s&p=d", j)) %>%
-      html_nodes('table') %>% .[[10]] %>% html_nodes('tr') %>%
-      html_nodes('td') %>% html_text()
+      html_nodes('table')
     
-    d <- data.frame(s[seq(from=1,to=length(s),by=2)], s[seq(from=2,to=length(s),
-                                                            by=2)])
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        j, which(x == j), length(x)
+      )
+    ) # Download message
+    
+    y = s %>% html_attr('class') == 
+      "js-snapshot-table snapshot-table2 screener_snapshot-table-body"
+    
+    l <- s[which(y)] %>% html_nodes('td') %>% html_text()
+    
+    d <- data.frame(
+      l[seq(from = 1, to = length(l), by = 2)], 
+      l[seq(from = 2, to = length(l), by = 2)]
+      ) # Form data frame
+    
     d[27,1] <- "EPS next Y (%)" # Change name
-      
+    
     rownames(d) <- d[,1] # Assign row names
-      
+    
     d <- subset(d, select = -c(1)) # Reduce excessive column
-      
+    
     colnames(d) <- j # Assign column name
-      
+    
     if (is.null(v)){ v <- d } else { v <- cbind(v, d) } } # Join
-      
+    
   v # Display
 }
 finviz.ratios(x = c("AAPL", "AMZN", "GOOGL", "META", "MSFT")) # Test
