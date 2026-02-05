15e99d460ae0ecbf91a8f8ce89dff1ade7dc86f5
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-02-05 11:20:18 +0500

    Update Correlation.R
    Updated script:
    
    • Edited script to suit it for Novabev stock data
    • Added time periods of observations for correlation

--- MOEX data/Correlation.R
+++ MOEX data/Correlation.R
@@ -3,27 +3,59 @@ lapply(c("moexer", "timeSeries", "xts"), require, character.only = T) # Libs
 moex.correlation <- function(x){ # Correlation coefficients for Russian stocks
   
   p <- NULL # Create an empty variable and get stock price data
+  l <- NULL # to store start and end dates for available trading days
   
   for (a in x){ 
     
-    D <- as.data.frame(get_candles(a, "2007-01-01", till = as.Date(Sys.Date()),
-                                   interval = 'daily')[,c(3,8)])
-    
-    message(
-      sprintf(
-        "%s is downloaded; %s from %s", 
-        a, which(x == a), length(x)
-      )
-    )
+    D = as.data.frame(get_candles(a,"2007-01-01",interval='daily')[,c(3,8)])
     
     D <- D[!duplicated(D),] # Remove duplicates
     
-    p <- cbind(p, xts(D[, 1], order.by = as.Date(D[, 2]))) }
+    D <- xts(D[,1], order.by = as.Date(D[,2])) # Move dates to row names
+    
+    D <- D[apply(D, 1, function(x) all(!is.na(x))),] # Get rid of NA
+    
+    colnames(D) <- x[n] # Put the tickers in data set
+    
+    D <- as.timeSeries(D) # Make it time series
+    
+    if (x[n] == "BELU"){ f <- which(rownames(D) == "2024-08-15")
+    
+      D[c(1:f),] <- D[c(1:f),] / 8 } # Adjustments for Novabev stock
+    
+    message(
+      sprintf(
+        "%s is downloaded (%s/%s)", 
+        a, which(x == a), length(x)
+      )
+    )
+    l <- rbind.data.frame(l, cbind(rownames(D)[1], rownames(D)[nrow(D)]))
+    
+    p <- cbind(p, D) }
   
-  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Eliminate NAs
+  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Eliminate NA
   
   colnames(p) <- x
   
-  cor(diff(log(as.timeSeries(p)))[-1,]) # returns matrix 
+  colnames(l) <- c("Start Date", "End Date")
+  rownames(l) <- x
+  
+  L <- list(
+    cor(diff(log(as.timeSeries(p)))[-1,]),
+    sprintf(
+      "From %s to %s.", 
+      rownames(p)[1],
+      rownames(p)[nrow(p)]
+      ),
+    l
+    ) # returns matrix 
+  
+  names(L) <- c(
+    "Correlation", 
+    "Time Period for all Securities", 
+    "Time Period for each security"
+    )
+  
+  L
 }
-moex.correlation(c("LKOH", "SBER", "MGNT"))
+moex.correlation(c("LKOH", "SBER", "MGNT", "BELU"))
