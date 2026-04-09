b4409118fef013baca225eb9199ec0fe61ce754c
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-09 13:28:13 +0500

    Refactor commodities.yahoo function for direct URL access
    Refactor commodities.yahoo function to remove parameter and directly read from the Yahoo Commodities URL. Update data extraction and column names accordingly.

--- Yahoo/Commodity Data.R
+++ Yahoo/Commodity Data.R
@@ -1,15 +1,24 @@
 library("rvest") # Library
 
-commodities.yahoo <- function(x){ # Data Frame with Commodity values
+commodities.yahoo <- function(){ # Data Frame with Commodity values
   
-  p <- read_html(x) %>% html_nodes('table') %>% html_nodes('tr') %>%
-    html_nodes('td') %>% html_text() # HTML
+  p1 <- read_html("https://finance.yahoo.com/commodities/") %>% 
+    html_nodes('table') %>% html_nodes('tr') %>% html_nodes('td') %>% 
+    html_nodes('div') # Read HTML
   
-  l <- data.frame(p[seq(from = 2, to = length(p), by = 9)],
-                  gsub(",", "", p[seq(from = 3, to = length(p), by = 9)]))
+  p <- p1 %>% html_nodes('span') %>% html_text() # Extract names 
   
-  colnames(l) <- c("Commodity", "Price") # Column names
+  v <- as.numeric(gsub(",", "", p[seq(from = 3, to = length(p), by = 3)]))
   
-  l # Display
+  tickers <- gsub(" ", "", p[seq(from = 1, to = length(p), by = 3)]) # Tickers
+  
+  p1 <- p1[seq(from = 3, to = length(p1), by = 6)] %>% html_text() # Names
+
+  df <- cbind.data.frame(p1, v) # merge names with values
+
+  rownames(df) <- tickers 
+  colnames(df) <- c("Commodity", "Points") # Column names
+
+  df # Display
 }
-commodities.yahoo("https://finance.yahoo.com/commodities/") # Test
+commodities.yahoo() # Test
