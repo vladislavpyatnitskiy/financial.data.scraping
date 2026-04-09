e7a480b9cee0d941a29b4b030d1927cb49986e64
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-09 14:28:40 +0500

    Update Crypto Data.R
    Updated script to handle new html format

--- Yahoo/Crypto Data.R
+++ Yahoo/Crypto Data.R
@@ -1,15 +1,22 @@
 library(rvest) # Library
 
-crypto.yahoo <- function(x){ # Data Frame with Cryptocurrency values
+crypto.yahoo <- function(){ # Data Frame with Crypto-currency values
   
-  p <- read_html(x) %>% html_nodes('table') %>% html_nodes('tr') %>%
-    html_nodes('td') %>% html_text() # HTML
+  p1 <- read_html("https://finance.yahoo.com/crypto/") %>% 
+    html_nodes('table') %>% html_nodes('tr') %>% html_nodes('td') %>% 
+    html_nodes('div') # HTML
   
-  l <- data.frame(p[seq(from = 1, to = length(p), by = 12)],
-                  gsub(",", "", p[seq(from = 3, to = length(p), by = 12)]))
+  p <- p1 %>% html_nodes('span') %>% html_text() # Extract names 
   
-  colnames(l) <- c("Cryptocurrency", "Price") # Column names
+  v <- as.numeric(gsub(",", "", p[seq(from = 4, to = length(p), by = 6)]))
   
-  l # Display
+  tickers <- gsub(" ", "", p[seq(from = 3, to = length(p), by = 6)]) # Tickers
+  
+  v <- as.data.frame(v) # 
+  
+  rownames(v) <- tickers
+  colnames(v) <- c("Price") # Column names
+  
+  v # Display
 }
-crypto.yahoo("https://finance.yahoo.com/crypto/") # Test
+crypto.yahoo() # Test
