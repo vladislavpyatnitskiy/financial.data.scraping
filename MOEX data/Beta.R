adb70de27e88c4ef1585557773272815a020e2e7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-14 13:17:50 +0500

    Refactor beta.moex
    Enhanced functionality by adding a list of redom and adjusting data handling for stock calculations.

--- MOEX data/Beta.R
+++ MOEX data/Beta.R
@@ -1,41 +1,99 @@
 lapply(c("moexer", "timeSeries", "xts"), require, character.only = T) # Libs 
 
-beta.moex <- function(y){ # Beta values for Russian stocks
+beta.moex <- function(x){ # Beta values for Russian stocks
   
-  y <- c(y, "IMOEX") # Add benchmark to list
+  x <- c(x, "IMOEX") # Add benchmark to list
+  
+  redom = list(
+    c("AGRO", "RAGR"), c("CIAN", "CNRU"), c("HHRU", "HEAD"), c("FIVE", "X5"),
+    c("FIXP", "FIXR"), c("YNDX", "YDEX"))
+  
+  from = "2007-01-01"
+  
+  J <- NULL
+  R <- NULL
+  
+  for (n in 1:length(x)){
     
-  p <- NULL # Empty variables for security values & Data upload
-  
-  start_date <- as.Date(Sys.Date() - 365 * 5) # Start Date
-  
-  for (A in y){ D <- as.data.frame(get_candles(A, start_date,
-                                               till = as.Date(Sys.Date()),
-                                               interval = 'daily')[,c(3,8)])
-
-    message(
-      sprintf(
-        "%s is downloaded; %s from %s", 
-        A, which(y == A), length(y)
+    if (any(sapply(redom, function(redom_item) x[n] %in% redom_item))){
+      
+      f <- which(sapply(redom, function(redom_item) x[n] %in% redom_item))
+      
+      for (k in 1:length(redom[[f]])){
+        
+        a = as.data.frame(
+          get_candles(redom[[f]][k], from=from, interval='daily')[,c(3,8)]
+        )
+        
+        if (k == 2){ 
+          
+          message(
+            sprintf(
+              "%s is downloaded; %s from %s", x[n], which(x == x[n]), length(x)
+            )
+          )
+        }
+        
+        a <- a[!duplicated(a),] # Remove duplicates
+        
+        a <- xts(a[, 1], order.by = as.Date(a[, 2]))
+        
+        if (x[n] == "AGRO") a <- a / 7.01
+        
+        colnames(a) <- redom[[f]][2]
+        
+        if (is.null(R)) R <- data.frame(a) else R <- rbind.data.frame(R, a)
+      }
+    } else {
+      
+      a = as.data.frame(get_candles(x[n], from=from, interval='daily')[,c(3,8)])
+      
+      message(
+        sprintf(
+          "%s is downloaded; %s from %s", 
+          x[n], which(x == x[n]), length(x)
+        )
       )
-    )
-  
-    D <- D[!duplicated(D),] # Remove duplicates
+      
+      a <- a[!duplicated(a),] # Remove duplicates
+      
+      a <- xts(a[, 1], order.by = as.Date(a[, 2]))
+      
+      colnames(a) <- x[n]
+      
+      R <- data.frame(a) 
+    }
     
-    p <- cbind(p, xts(D[, 1], order.by = as.Date(D[, 2]))) }
-  
-  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  colnames(p) <- y # Put the tickers in data set
+    R <- as.timeSeries(R) # Make it time series
     
-  p <- diff(log(as.timeSeries(p)))[-1,] # Time Series Returns and NA off
+    if (x[n] == "BELU"){ j <- which(rownames(R) == "2024-08-15")
     
-  B <- apply(p[, -which(names(p) == "IMOEX")], 2,
-             function(col) ((lm((col) ~ p[,"IMOEX"]))$coefficients[2]))
+      R[c(1:j),] <- R[c(1:j),]/8 } # Adjustments for Novabev stock
+    
+    if (is.null(J)) J <- list(R) else J[[n]] <- R 
+    R <- NULL  # Reset R for next iteration
+  }
   
-  B <- as.data.frame(round(B, 2)) # Round by 2 decimal numbers
+  B <- NULL
   
+  p2 <- as.timeSeries(J[[length(J)]])
+  
+  for (n in 1:(length(J) - 1)){
+
+    p <- cbind(as.timeSeries(J[[n]]), p2)
+    
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+    
+    p <- diff(log(as.timeSeries(p)))[-1,] # Time Series Returns and NA off
+
+    B <- rbind.data.frame(
+      B, round(lm((p[,1]) ~ p[,"IMOEX"])$coefficients[2], 2)
+      ) # Round by 2 decimal numbers
+  }
+
+  rownames(B) <- x[-length(x)]
   colnames(B) <- "Beta" # Column name
-  
+
   return(B) # Display values
 }
 beta.moex(c("LKOH", "RASP")) # Test
