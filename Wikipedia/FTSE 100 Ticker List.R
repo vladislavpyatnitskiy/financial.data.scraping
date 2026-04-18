708d639ff024184ac43c1cf1f6f468db9f2611a9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-18 12:42:31 +0500

    Refactor FTSE 100 ticker scraping function
    Updated the function to scrape FTSE 100 tickers from Wikipedia and added a data frame for the output.

--- Wikipedia/FTSE 100 Ticker List.R
+++ Wikipedia/FTSE 100 Ticker List.R
@@ -2,11 +2,22 @@ library("rvest") # Library
 
 ftse100.list.wiki <- function(x, yahoo = T){ # Tickers from FTSE 100
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
-    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
-    html_nodes('td') %>% html_text() # HTML to get data
+  f <- read_html("https://en.wikipedia.org/wiki/FTSE_100_Index") |> 
+    html_element("#constituents") %>% html_nodes('tr') %>% html_nodes('td') %>% 
+    html_text()
   
-  if (isTRUE(yahoo)){ paste(s[seq(from=2, to=length(s), by=3)], ".L", sep="")
-    } else { s[seq(from = 2, to = length(s), by = 3)] } # Display
+  if (yahoo) return(paste(f[seq(from=2, to=length(f), by=3)], ".L", sep=""))
+  
+  df <- data.frame(
+    f[seq(from = 1, to = length(f), by = 3)],
+    f[seq(from = 2, to = length(f), by = 3)],
+    f[seq(from = 3, to = length(f), by = 3)]
+  )
+  
+  colnames(df) <- c("Company Name", "Ticker", "Sector")
+  
+  df[,3] <- gsub("\n", "", df[,3])
+  
+  df
 }
-ftse100.list.wiki("FTSE_100_Index", yahoo = T) # Test
+ftse100.list.wiki("FTSE_100_Index", yahoo = F) # Test
