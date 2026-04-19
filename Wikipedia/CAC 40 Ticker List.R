0d0e965f9990b1e81570153651de1e8b7ddefd07
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-19 12:55:17 +0500

    Refactor CAC 40 ticker list function parameters

--- Wikipedia/CAC 40 Ticker List.R
+++ Wikipedia/CAC 40 Ticker List.R
@@ -1,11 +1,24 @@
 library("rvest") # Library
 
-cac40.list.wiki <- function(x){ # Tickers from CAC 40
+cac40.list.wiki <- function(yahoo=F){ # Tickers from CAC 40
+   
+  f <- read_html("https://en.wikipedia.org/wiki/CAC_40") |> 
+    html_element("#constituents") %>% html_nodes('tr') %>% html_nodes('td') %>% 
+    html_text() # Get data
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
-    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
-    html_nodes('td') %>% html_text() # Get HTML for index data with tickers
+  tickers <- gsub("\n", "", f[seq(from=4, to=length(f), by=4)])
   
-  gsub('["\n"]', '', s[seq(from = 4, to = length(s), by = 4)]) # Display
+  if (yahoo) return(tickers) # When you need only tickers
+  
+  df <- data.frame(
+    f[seq(from = 1, to = length(f), by = 4)], # Company Names
+    f[seq(from = 2, to = length(f), by = 4)], # Sector
+    f[seq(from = 3, to = length(f), by = 4)], # Industry
+    tickers # Tickers
+  ) 
+   
+  colnames(df) <- c("Company Name", "Sector", "Industry", "Ticker")
+   
+  df # Display
 }
-cac40.list.wiki("CAC_40") # Test
+cac40.list.wiki(T) # Test 
