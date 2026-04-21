ea1f94c32286168700d814a79626871f75217dfa
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-21 11:33:32 +0500

    Refactor dowjones.list.wiki function for improved data retrieval
    Updated the function to fetch Dow Jones tickers from the Wikipedia page. Added options for returning only tickers or a data frame with company names and sectors.

--- Wikipedia/Dow Jones Industrial Average List.R
+++ Wikipedia/Dow Jones Industrial Average List.R
@@ -1,11 +1,26 @@
 library("rvest") # Library
 
-dowjones.list.wiki <- function(x){ # Show list of tickers from DJIA
+dowjones.list.wiki <- function(yahoo=T){ # Show list of tickers from DJIA
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
-    html_nodes('table') %>% .[[2]] %>% html_nodes('tr') %>%
-    html_nodes('td') %>% html_text() # Get HTML for index data with tickers
+  D <- read_html(
+    "https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average") |> 
+    html_element("#constituents") %>% html_nodes('tr') 
   
-  gsub('["\n"]', '', s[seq(from = 2, to = length(s), by = 6)]) # Display
+  dow_values <- gsub("[\n]", "", D %>% html_nodes('td') %>% html_text())
+  dow_names <- gsub("[\n]", "", D %>% html_nodes('th') %>% html_text())
+  
+  tickers <- dow_values[seq(from = 2, to = length(dow_values), by = 6)]
+  
+  if (yahoo) return(tickers) # When you need only tickers
+  
+  df <- data.frame(
+    tickers, # Tickers
+    dow_names[which(dow_names == "3M"):length(dow_names)], # Company Names
+    dow_values[seq(from = 3, to = length(dow_values), by = 6)] # Sector
+  ) 
+  
+  colnames(df) <- c( "Ticker", "Company Name", "Sector")
+  
+  df # Display
 }
-dowjones.list.wiki("Dow_Jones_Industrial_Average") # Test
+dowjones.list.wiki(F) # Test
