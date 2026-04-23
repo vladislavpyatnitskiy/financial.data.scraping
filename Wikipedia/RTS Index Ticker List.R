e78783bcd71bceac0fdce83382929d8ff2327aef
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-23 15:07:09 +0500

    Refactor rtsi.list.wiki to accept boolean parameter
    Updated rtsi.list.wiki function to improve data retrieval and flexibility.

--- Wikipedia/RTS Index Ticker List.R
+++ Wikipedia/RTS Index Ticker List.R
@@ -1,11 +1,38 @@
 library("rvest") # library
 
-rtsi.list.wiki <- function(x){ # Tickers from RTS Index
+rtsi.list.wiki <- function(yahoo=T){ # Tickers from RTS Index
   
-  s <- read_html(paste("https://ru.wikipedia.org/wiki/", x, sep = "")) %>%
-    html_nodes('table') %>% .[[5]] %>% html_nodes('tr') %>%
-    html_nodes('td') %>% html_text() # Get HTML for index data with tickers
+  f <- read_html("https://ru.wikipedia.org/wiki/Индекс_РТС") %>% 
+    html_nodes('table')
   
-  gsub('["\n"]', '', s[seq(from = 2, to = length(s), by = 7)]) # Display
+  l <- NULL
+
+  for (n in 1:length(f)){ # Derive ticker and price data
+
+    if (isTRUE(f[n] %>% html_attr('class') == "wikitable sortable")){
+
+      l <- c(l, f[n])
+    }
+  }
+  D <- l %>% .[[1]] %>% html_nodes('tbody') %>% html_nodes('tr') %>% 
+    html_nodes('td') %>% html_text()
+  
+  df <- gsub("[\n]", "", D)
+  
+  tickers <- df[seq(from = 2, to = length(df), by = 7)]
+  
+  if (yahoo) return(tickers) # When you need only tickers
+  
+  pct <- gsub("%", "", gsub(",", ".", df[seq(from=7, to=length(df), by=7)]))
+  
+  df <- data.frame(
+    tickers, # Tickers
+    gsub("\\", "", df[seq(from=3, to=length(df), by=7)], fixed=T),
+    pct # Weight
+  )
+  
+  colnames(df) <- c( "Ticker", "Company Name", "Weight (%)")
+  
+  df # Display
 }
-rtsi.list.wiki("Индекс_РТС") # Test
+rtsi.list.wiki(T) # Test
