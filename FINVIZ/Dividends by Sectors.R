1a4f9626260273671a6534d8bfab439c1f756bf1
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-08-13 11:40:29 +0500

    Refactor finviz.sectors.dividends for improved data handling
    Updated finviz.sectors.dividends function to remove parameter and directly fetch sector data from a new URL. Improved data processing and ensured numeric conversion for dividend yield values.

--- FINVIZ/Dividends by Sectors.R
+++ FINVIZ/Dividends by Sectors.R
@@ -1,26 +1,33 @@
 library("rvest") # Library
 
-finviz.sectors.dividends <- function(x){ # Dividends Yield for each sector
+finviz.sectors.dividends <- function(){ # Dividends Yield for each sector
   
-  s <- read_html(sprintf("https://finviz.com/%s", x)) %>%
-    html_nodes('table') %>% .[[8]] %>% html_nodes('tr') %>%
+  s <- read_html("https://finviz.com/groups.ashx?g=sector&v=110&o=name") %>%
+    html_nodes('table') %>% .[[8]] %>% html_nodes('tr') %>% 
     html_nodes('td') %>% html_text() # Get data
+
+  d <- data.frame(
+    s[seq(from = 2, to = length(s), by = 14)], 
+    s[seq(from = 5, to = length(s), by = 14)]
+    )
   
-  d <- data.frame(s[seq(from=2,to=length(s),by=11)], s[seq(from=5,to=length(s),
-                                                          by=11)])
   rownames(d) <- d[,1] # Assign row names
-  
+
   d <- subset(d, select = -c(1)) # Reduce excessive column
-  
+
   colnames(d) <- c("Dividend Yield (%)") # Assign Column name
-  
+
   for (n in 1:nrow(d)){ # Reduce "%" from Dividend Yield column values
-    
-    d[n,1] <- read.fwf(textConnection(d[n,1]), widths = c(nchar(d[n,1]) - 1, 1),
-                       colClasses = "character")[,1] }
-  
+
+    d[n,1] <- read.fwf(
+      textConnection(d[n,1]), 
+      widths = c(nchar(d[n,1]) - 1, 1),
+      colClasses = "character"
+      )[,1] 
+  }
+
   for (n in 1:ncol(d)){ d[,n] <- as.numeric(d[,n]) } # Make data numeric
-  
+
   d # Display
 }
-finviz.sectors.dividends("groups.ashx?g=sector&v=110&o=name") # Test
+finviz.sectors.dividends() # Test
