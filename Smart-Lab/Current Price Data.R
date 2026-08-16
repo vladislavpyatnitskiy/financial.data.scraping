f46421923f2f3e9cf42fb181fb0e289cbba20ce9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-08-16 11:11:39 +0500

    Refactor smartlab.info to remove input parameter
    Updated smartlab.info function to eliminate the input parameter, directly fetching data from the URL.

--- Smart-Lab/Current Price Data.R
+++ Smart-Lab/Current Price Data.R
@@ -1,17 +1,18 @@
 library("rvest") # library
 
-smartlab.info <- function(x){ # Current Stock Price Data
+smartlab.info <- function(){ # Current Stock Price Data
   
-  f <- read_html(x) %>% html_nodes('table') %>% .[[1]] %>% html_nodes('tr') 
+  f <- read_html("https://smart-lab.ru/q/shares/") %>% 
+    html_nodes('table') %>% .[[1]] %>% html_nodes('tr') 
   
   L <- NULL # Reorganise data into data frame
   
   for (n in 2:length(f)){ l <- f[n] %>% html_nodes('td') %>% html_text()
   
-    L <- rbind.data.frame(L, cbind(l[3], l[7])) } # Info about ticker and price
+  L <- rbind.data.frame(L, cbind(l[3], l[7])) } # Info about ticker and price
   
   colnames(L) <- c("ticker", "price") # Column names
   
   L # Display
 }
-smartlab.info("https://smart-lab.ru/q/shares/") # Test
+smartlab.info() # Test
